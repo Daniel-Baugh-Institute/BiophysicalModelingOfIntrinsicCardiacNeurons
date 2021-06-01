7d503d9c1e2d09f117e220fbc44236a0e1a34ead
Jessica Feldman <jessfeld@gmail.com>
2021-06-01 11:05:33 -0400

    update to batch and netParams 2x3

--- batch.py
+++ batch.py
@@ -3,11 +3,11 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        params['amp'] =   [0.05, 0.1, 0.3, 0.5, 0.7, 1.0]
-        params['cellnum'] = [x for x in range(115)]
+        params['amp'] =   [0.05, 0.1, 0.3]
+        params['cellnum'] =  [101, 115]#[x for x in range(115)]
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams.py',)
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21may31a'
+        b.batchLabel = '21june01a'
         b.saveFolder = 'data'
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
