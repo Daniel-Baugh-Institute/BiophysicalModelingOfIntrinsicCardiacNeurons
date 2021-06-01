02612d9280ca4032e2bdd851451def5e96d71dfa
Jessica Feldman <jessfeld@gmail.com>
2021-06-01 15:25:15 -0400

    updating batch.py

--- batch.py
+++ batch.py
@@ -4,8 +4,8 @@ from netpyne.batch import Batch
 def batch():
         params = specs.ODict()
         params['amp'] =   [0.05, 0.1, 0.3]
-        params['cellnum'] =  [101, 115]#[x for x in range(115)]
-        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams.py',)
+        params['cellnum'] =  [x for x in range(2)]
+        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams.py')
         # Set output folder, grid method (all param combinations), and run configuration
         b.batchLabel = '21june01a'
         b.saveFolder = 'data'
