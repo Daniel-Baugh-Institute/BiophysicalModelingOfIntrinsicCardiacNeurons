1d3666def60166b437112365e72e1662a1417e2f
Jessica Feldman <jessfeld@gmail.com>
2021-06-09 12:36:13 -0400

    rmp in init.py

--- init.py
+++ init.py
@@ -2,3 +2,18 @@ from netpyne import sim
 
 simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams.py')
 sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)
+print('AAAAAAAAAAAAAAAAAAAAAA')
+
+
+def set_memb():
+    isum = 0.0
+    if ismembrane("na_ion"):
+        isum = isum + ina  
+    if ismembrane("k_ion"):
+        isum = isum + ik    
+    if ismembrane("ca_ion"):
+        isum = isum + ica  
+    if ismembrane("iother"):
+        isum = isum + iother 
+
+
