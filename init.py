cf0948f2c6e9fc866bf15f9eb9b1b24cff93c7d7
Bill Lytton <wwlytton@yahoo.com>
2021-06-09 13:13:00 -0400

    fix args

--- init.py
+++ init.py
@@ -1,19 +1,10 @@
 from netpyne import sim
 
 simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams.py')
-sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)
+# sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)
+sim.create(netParams=netParams, simConfig=simConfig)
+
 print('AAAAAAAAAAAAAAAAAAAAAA')
 
-
 def set_memb():
-    isum = 0.0
-    if ismembrane("na_ion"):
-        isum = isum + ina  
-    if ismembrane("k_ion"):
-        isum = isum + ik    
-    if ismembrane("ca_ion"):
-        isum = isum + ica  
-    if ismembrane("iother"):
-        isum = isum + iother 
-
-
+    isum =  = ina + ik + ica + iother
