149065f9103531a16c7889452c61e85486494365
Jessica Feldman <jessfeld@gmail.com>
2021-06-11 14:13:58 -0400

    updating init.py

--- init.py
+++ init.py
@@ -5,15 +5,16 @@ simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsD
 sim.create(netParams=netParams, simConfig=simConfig)
 
 
-def set_memb(vinit):
+def set_memb():
     '''set steady state RMP for 1 cell'''
+    vinit = sim.net.cells[0].secs.soma.hObj(0.5).v
     seg = sim.net.cells[0].secs.soma.hObj(0.5) # since only 1 cell with nseg=1 can jump straight to that seg
-    isum = ina + ik + ica + iother
-    e_pas= vinit + isum/g_pas
-    if e_pas<-100:
-        g_pas = isum/(e_pas - vinit)
+    isum = seg.ina + seg.ik + seg.ica + seg.iother
+    seg.e_pas= vinit + isum/seg.g_pas
+    if seg.e_pas < -100:
+        seg.g_pas = isum/(seg.e_pas - vinit)
     else:
         print("Error!")
 
-
+set_memb()
 print('AAAAAAAAAAAAAAAAAAAAAA')
