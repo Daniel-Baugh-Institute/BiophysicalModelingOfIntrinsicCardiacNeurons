ce2c3bac64683e6a53e87c1461efd2c85997baf0
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-06-11 17:31:51 -0400

    Example code Updated to RMD's python code

--- fhexample.py
+++ fhexample.py
@@ -1,46 +1,43 @@
-from neuron import h
+# specify an example model
+from neuron import h, gui
 
-h.load_file("nrngui.hoc")
+a = h.Section()
 b = h.Section()
-b.insert('hh')
 
+for sec in h.allsec():
+    sec.insert('hh')
 
 def fi0():
-	print ("fi0() called after v set but before INITIAL blocks")
-	print ("  b.v=%g b.m_hh=%g\n", b.v, b.m_hh)
-	b.v = 10
-
+    print('fi0 called after v set but before INITIAL blocks')
+    print('  a.v=%g a.m_hh=%g' % (a.v, a.m_hh))
+    a.v = 10
 
 def fi1():
-	print ("fi1() called after INITIAL blocks but before BREAKPOINT blocks")
-	print ("     or variable step initialization.")
-	print ("     Good place to change any states.")
-	print ("  b.v=%g b.m_hh=%g\n", b.v, b.m_hh)
-	b.v = 10
+    print('fi1() called after INITIAL blocks but before BREAKPOINT blocks')
+    print('     or variable step initialization.')
+    print('     Good place to change any states.')
+    print('  a.v=%g a.m_hh=%g' % (a.v, a.m_hh))
+    print('  b.v=%g b.m_hh=%g' % (b.v, b.m_hh))
+    b.v = 10
 
 def fi2():
-	print ("fi2() called after everything initialized. Just before return")
-	print ("     from finitialize.")
-	print ("     Good place to record or plot initial values")
-	print ("  b.v=%g b.m_hh=%g\n", b.v, b.m_hh)
+    print('fi2() called after everything initialized. Just before return')
+    print('     from finitialize.')
+    print('     Good place to record or plot initial values')
+    print('  a.v=%g a.m_hh=%g' % (a.v, a.m_hh))
+    print('  b.v=%g b.m_hh=%g' % (b.v, b.m_hh))
 
+fih = [h.FInitializeHandler(0, fi0),
+       h.FInitializeHandler(1, fi1),
+       h.FInitializeHandler(2, fi2)]
 
-fih0 = h.FInitializeHandler(0, "fi0()")
-fih1 = h.FInitializeHandler(1, "fi1()")
-fih2 = h.FInitializeHandler(2, "fi2()")
+class Test:
+    def __init__(self):
+        self.fih = h.FInitializeHandler(self.p)
+    def p(self):
+        print('inside %r.p()' % self)
 
-# begintemplate Test
-# objref fih, this
-# proc init() {
-# 	fih = new FInitializeHandler("p()", this)
-# }
-# proc p() {
-# 	printf("inside %s.p()\n", this)
-# }
-# endtemplate Test
-
-# objref test
-# test = new Test()
+test = Test()
 
 h.stdinit()
-fih1.allprint()
+fih[0].allprint()
