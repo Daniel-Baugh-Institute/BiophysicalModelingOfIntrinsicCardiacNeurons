ce0d6bf9d49a9da34bcfedfe8a9fd6f5eedf68bd
Bill Lytton <wwlytton@yahoo.com>
2021-06-11 14:28:40 -0400

    put in another printout

--- init.py
+++ init.py
@@ -7,6 +7,7 @@ sim.create(netParams=netParams, simConfig=simConfig)
 
 def set_memb(vinit=-61):
     '''set steady state RMP for 1 cell'''
+    print('BBBBBBBBBBBBBBBB')
     seg = sim.net.cells[0].secs.soma.hObj(0.5) # since only 1 cell with nseg=1 can jump straight to that seg
     isum = seg.ina + seg.ik + seg.ica + seg.iother
     seg.e_pas= vinit + isum/seg.g_pas
