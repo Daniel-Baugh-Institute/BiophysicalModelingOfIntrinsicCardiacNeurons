910bbc1c423220d0c322464fc4f6456f2bfcbb59
Bill Lytton <wwlytton@yahoo.com>
2021-06-11 11:41:28 -0400

    find the seg that we're going to work with

--- init.py
+++ init.py
@@ -5,13 +5,15 @@ simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsD
 sim.create(netParams=netParams, simConfig=simConfig)
 
 
-def set_memb():
+def set_memb(vinit):
+    '''set steady state RMP for 1 cell'''
+    seg = sim.net.cells[0].secs.soma.hObj(0.5) # since only 1 cell with nseg=1 can jump straight to that seg
     isum = ina + ik + ica + iother
-    e_pas= h.v_init + isum/g_pas
+    e_pas= vinit + isum/g_pas
     if e_pas<-100:
-        g_pas = isum/(e_pas-h.v_init)
+        g_pas = isum/(e_pas - vinit)
     else:
         print("Error!")
 
-set_memb()
+
 print('AAAAAAAAAAAAAAAAAAAAAA')
