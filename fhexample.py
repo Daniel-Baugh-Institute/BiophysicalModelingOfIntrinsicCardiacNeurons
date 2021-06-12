83e9143b96208aeddddc2ff8e60e11263751b161
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-06-11 20:14:42 -0400

    setting rmp - hoc error

--- fhexample.py
+++ fhexample.py
@@ -1,11 +1,13 @@
 # specify an example model
-from neuron import h, gui
+from neuron import h#, gui
 
 a = h.Section()
 b = h.Section()
 
 for sec in h.allsec():
     sec.insert('hh')
+    sec.insert('pas')
+    sec.gnabar_hh = 10
 
 def fi0():
     print('fi0 called after v set but before INITIAL blocks')
@@ -20,16 +22,19 @@ def fi1():
     print('  b.v=%g b.m_hh=%g' % (b.v, b.m_hh))
     b.v = 10
 
-def fi2():
-    print('fi2() called after everything initialized. Just before return')
-    print('     from finitialize.')
-    print('     Good place to record or plot initial values')
-    print('  a.v=%g a.m_hh=%g' % (a.v, a.m_hh))
-    print('  b.v=%g b.m_hh=%g' % (b.v, b.m_hh))
+def fi2(vinit):
+    isum = a.ina + a.ik
+    a.e_pas= vinit + isum/a.g_pas
+    # print('fi2() called after everything initialized. Just before return')
+    # print('     from finitialize.')
+    # print('     Good place to record or plot initial values')
+    # print('  a.v=%g a.m_hh=%g' % (a.v, a.m_hh))
+    # print('  b.v=%g b.m_hh=%g' % (b.v, b.m_hh))
+
 
 fih = [h.FInitializeHandler(0, fi0),
        h.FInitializeHandler(1, fi1),
-       h.FInitializeHandler(2, fi2)]
+       h.FInitializeHandler(2, fi2(-61))]
 
 class Test:
     def __init__(self):
@@ -40,4 +45,4 @@ class Test:
 test = Test()
 
 h.stdinit()
-fih[0].allprint()
+fih[2].allprint()
