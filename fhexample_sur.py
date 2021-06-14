caff73efda44919c3a56cc1166007b68c80520eb
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-06-14 15:33:14 -0400

    Replaced pas with HH's leak - working

--- fhexample_sur.py
+++ fhexample_sur.py
@@ -9,43 +9,43 @@ h.v_init = -61
 
 for sec in h.allsec():
     sec.insert('hh')
-    sec.insert('pas')
-    sec.gnabar_hh = 10
+    sec.gnabar_hh = 10 #(default is 0.12)
+    # sec.gkbar_hh = 10   #(default is 0.036)
+    #tried - 10, 0.4 and 0.04
 
 def fi():
-    print('epas at begg. of fi() = ', a.e_pas)
+    print('eleak at begg. of fi() = ', a.el_hh)
     isum = a.ina + a.ik
-    # a.i_pas = a.g_pas*(h.v_init-a.e_pas)
     if isum==0:
-        a.e_pas = h.v_init
+        a.el_hh = h.v_init
     else:
-        if a.g_pas>0:
-            a.e_pas = h.v_init+isum/a.g_pas
+        if a.gl_hh>0:
+            a.el_hh = h.v_init+isum/a.gl_hh
         else:
-            if a.e_pas != h.v_init:
-                a.g_pas = isum/(a.e_pas-h.v_init)
+            if a.el_hh != h.v_init:
+                a.gl_hh = isum/(a.el_hh-h.v_init)
     print('isum = ',isum)
-    print('ipas = ',a.i_pas)
-    print('epas = ',a.e_pas)
+    print('ileak = ',a.il_hh)
+    print('isum+ileak =',isum+a.il_hh)
+    print('eleak = ',a.el_hh)
 
 
 fih = [h.FInitializeHandler(2, fi)]
 
 volt = h.Vector().record(a(0.5)._ref_v)            
 time = h.Vector().record(h._ref_t) 
-# epas = h.Vector().record(a(0.5)._ref_e_pas) 
 
 # h.stdinit()
 fih[0].allprint()
 h.finitialize(h.v_init)
 h.fcurrent()
-h.tstop = 10*ms
+h.tstop = 5*ms
 h.run(h.tstop)
-print('Volt at 5 ms = ',volt[-1])
+print('Volt at 0 ms = ',volt[0])
+print('Volt at 10 ms = ',volt[-1])
 
 plt.figure()
 plt.plot(time, volt)
-# plt.plot(time,epas)
 plt.xlabel('t (ms)')
 plt.ylabel('v (mV)')
 plt.show()
