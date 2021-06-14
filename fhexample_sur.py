e882cf0228067a2da7caece78834b06e48bc0ada
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-06-14 12:41:14 -0400

    plotting ss - code still failing

--- fhexample_sur.py
+++ fhexample_sur.py
@@ -1,5 +1,7 @@
 # specify an example model
 from neuron import h
+from neuron.units import ms, mV
+import matplotlib.pyplot as plt
 h.load_file('stdrun.hoc')
 
 a = h.Section()
@@ -20,7 +22,17 @@ def fi():
 
 fih = [h.FInitializeHandler(1, fi)]
 
+v = h.Vector().record(a(0.5)._ref_v)            
+t = h.Vector().record(h._ref_t) 
 
 h.stdinit()
 fih[0].allprint()
 h.finitialize(h.v_init)
+
+h.continuerun(10*ms)
+
+plt.figure()
+plt.plot(t, v)
+plt.xlabel('t (ms)')
+plt.ylabel('v (mV)')
+plt.show()
