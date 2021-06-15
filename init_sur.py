c2653d265e2ce971da75f6d679cb48112ec94de8
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-06-15 11:14:29 -0400

    Implementing rmp() in the netpyne code

---
+++ init_sur.py
@@ -0,0 +1,30 @@
+from netpyne import sim
+from neuron import h
+
+simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams.py')
+sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)
+# sim.create(netParams=netParams, simConfig=simConfig)
+
+
+def fi():
+    '''set steady state RMP for 1 cell'''
+    seg = sim.net.cells[0].secs.soma.hObj(0.5) # since only 1 cell with nseg=1 can jump straight to that seg
+    print('epas at begg. of fi() = ', seg.e_pas)
+    isum = seg.ina + seg.ik + seg.ica + seg.iother
+    if isum==0:
+        seg.e_pas = cfg.hParams['v_init']
+    else:
+        if seg.g_pas>0:
+            seg.e_pas = cfg.hParams['v_init']+isum/seg.g_pas
+        else:
+            if seg.e_pas != h.v_init:
+                seg.g_pas = isum/(seg.e_pas-cfg.hParams['v_init'])
+    print('isum = ',isum)
+    print('ipas = ',seg.i_pas)
+    print('isum+ipas =',isum+seg.i_pas)
+    print('epas = ',seg.e_pas)
+
+print('AAAAAAAAAAAAAAAAAAAAAA')
+
+fih = [h.FInitializeHandler(2, fi)]
+
