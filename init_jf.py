4944863d80905959df9f1b02b2d07532d97571c5
Jessica Feldman <jessfeld@gmail.com>
2021-06-15 17:46:55 -0400

    adding init_jf.py

--- init_jf.py
+++ init_jf.py
@@ -1,56 +1,44 @@
 from netpyne import sim
 from neuron import h
-h.load_file('stdrun.hoc')
 import matplotlib.pyplot as plt
-
 
 
 simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams.py')
 # sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)
 sim.create(netParams=netParams, simConfig=simConfig)
 
-'''set steady state RMP for 1 cell'''
-
-h.v_init = -61
-
 
 def fi():
-    seg = sim.net.cells[0].secs.soma.hObj(0.5) # since only 1 cell with nseg=1 can jump straight to that segt('epas at begg. of fi() = ', seg.e_pas)
-    isum = seg.ina + seg.ik + seg.il_hh + seg.iother + seg.ica
+    '''set steady state RMP for 1 cell'''
+    print('AAAAAAAAAAAAAAAAAAAAAA')
+    seg = sim.net.cells[0].secs.soma.hObj(0.5) # since only 1 cell with nseg=1 can jump straight to that seg
+    print('epas at begg. of fi() = ', seg.e_pas)
+    isum = seg.ina + seg.ik + seg.ica + seg.iother
     if isum==0:
-        seg.e_pas = h.v_init
+        seg.e_pas = cfg.hParams['v_init']
     else:
         if seg.g_pas>0:
-           seg.e_pas = h.v_init+isum/seg.g_pas
+            seg.e_pas = cfg.hParams['v_init']+isum/seg.g_pas
         else:
-            if seg.e_pas != h.v_init:
-                seg.g_pas = isum/(seg.e_pas-h.v_init)
+            if seg.e_pas != cfg.hParams['v_init']:
+                seg.g_pas = isum/(seg.e_pas-cfg.hParams['v_init'])
     print('isum = ',isum)
-    print('ipas = ', seg.i_pas)
-    print('isum+ipas =',isum + seg.i_pas)
+    print('ipas = ',seg.i_pas)
+    print('isum+ipas =',isum+seg.i_pas)
     print('epas = ',seg.e_pas)
-    volt = h.Vector().record(seg.v) 
-    time = h.Vector().record(h._ref_t)           
-    
+    print(seg.v)
+    print(h.t)
 
-
-fih = [h.FInitializeHandler(2,fi)]
-fih[0].allprint()
-h.finitialize(h.v_init)
-
-
-# h.stdinit()
-
-h.fcurrent()
-h.tstop = 5
-h.run(h.tstop)
-print('Volt at 0 ms = ',volt[0])
-print('Volt at 10 ms = ',volt[-1])
+fih = [h.FInitializeHandler(2, fi)]
+sim.simulate()
+sim.analyze()
+print(seg.v)
 
 plt.figure()
-plt.plot(time, volt)
+plt.plot(seg.v,h.t)
 plt.xlabel('t (ms)')
-plt.ylabel('v (mV)')
+plt.ylabel('v (mV)') 
 plt.show()
+
 
 
