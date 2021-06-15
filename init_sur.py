7256b5d3c8a119f8476259ec771222948fd3fb49
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-06-15 16:45:32 -0400

    moved sim.simulate() to after FH and added analyze()

--- init_sur.py
+++ init_sur.py
@@ -4,7 +4,6 @@ from neuron import h
 simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams.py')
 # sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)
 sim.create(netParams=netParams, simConfig=simConfig)
-sim.simulate()
 
 
 def fi():
@@ -25,6 +24,10 @@ def fi():
     print('ipas = ',seg.i_pas)
     print('isum+ipas =',isum+seg.i_pas)
     print('epas = ',seg.e_pas)
+    print(seg.v)
+    print(h.t)
 
 fih = [h.FInitializeHandler(2, fi)]
 
+sim.simulate()
+sim.analyze()
