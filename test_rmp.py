bdbee9ad114c2338b9e8f4801fed12785ff8c2ab
Jessica Feldman <jessfeld@gmail.com>
2021-06-17 13:55:22 -0400

    adding new test_rmp script

--- test_rmp.py
+++ test_rmp.py
@@ -1,3 +1,19 @@
 #purpose: to show rmp. Creates scatter plot w/ one point for the rmp for each of 115 cellnums
 #load json file to extract v and time
 #(y1-y0)/(x1-x0)
+import json
+import pandas as pd
+#import seaborn as sb
+import matplotlib.pyplot as plt
+import numpy as np
+from pylab import *
+from itertools import product
+from pprint import pprint
+from netpyne import specs
+from collections import OrderedDict
+
+filename = []
+
+
+with open(filename, 'r') as fileObj:
+            dataLoad = json.load(fileObj, object_pairs_hook=OrderedDict)
