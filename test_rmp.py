de5caa9a71af18adb3206d889156299bdefdf3d4
Jessica Feldman <jessfeld@gmail.com>
2021-06-17 16:13:23 -0400

    fsdfs

--- test_rmp.py
+++ test_rmp.py
@@ -12,92 +12,8 @@ from netpyne import specs
 from collections import OrderedDict
 from pprint import pprint
 #import seaborn as sb <<< deprecated 
+from analysis.py import toPandas
 
-#--------------------------------------------------------------------
-# Function to read batch data
-#--------------------------------------------------------------------
-def readBatchData(dataFolder, batchLabel, loadAll=False, saveAll=True, vars=None, maxCombs=None, listCombs=None):
-    # load from previously saved file with all data
-    if loadAll:
-        print('\nLoading single file with all data...')
-        filename = '%s/%s/%s_allData.json' % (dataFolder, batchLabel, batchLabel)
-        with open(filename, 'r') as fileObj:
-            dataLoad = json.load(fileObj, object_pairs_hook=OrderedDict)
-        params = dataLoad['params']
-        data = dataLoad['data']
-        return params, data
-
-    if isinstance(listCombs, str):
-        filename = str(listCombs)
-        with open(filename, 'r') as fileObj:
-            dataLoad = json.load(fileObj)
-        listCombs = dataLoad['paramsMatch']
-
-    # read the batch file and cfg
-    batchFile = '%s/%s_batch.json' % (dataFolder, batchLabel)
-    with open(batchFile, 'r') as fileObj:
-        b = json.load(fileObj)['batch']
-
-    # read params labels and ranges
-    params = b['params']
-
-    # reorder so grouped params come first
-    preorder = [p for p in params if 'group' in p and p['group']]
-    for p in params:
-        if p not in preorder: preorder.append(p)
-    params = preorder
-
-    # read vars from all files - store in dict
-    if b['method'] == 'grid':
-        labelList, valuesList = list(zip(*[(p['label'], p['values']) for p in params]))
-        valueCombinations = product(*(valuesList))
-        indexCombinations = product(*[list(range(len(x))) for x in valuesList])
-        data = {}
-        print('Reading data...')
-        missing = 0
-        for i,(iComb, pComb) in enumerate(zip(indexCombinations, valueCombinations)):
-            if (not maxCombs or i<= maxCombs) and (not listCombs or list(pComb) in listCombs):
-                print(i, iComb)
-                # read output file
-                iCombStr = ''.join([''.join('_'+str(i)) for i in iComb])
-                simLabel = b['batchLabel']+iCombStr
-                outFile = b['saveFolder']+'/'+simLabel+'.json'
-                try:
-                    with open(outFile, 'r') as fileObj:
-                        output = json.load(fileObj, object_pairs_hook=OrderedDict)
-                    # save output file in data dict
-                    data[iCombStr] = {}
-                    data[iCombStr]['paramValues'] = pComb  # store param values
-                    if not vars: vars = list(output.keys())
-
-                    for key in vars:
-                        if isinstance(key, tuple):
-                            container = output
-                            for ikey in range(len(key)-1):
-                                container = container[key[ikey]]
-                            data[iCombStr][key[1]] = container[key[-1]]
-
-                        elif isinstance(key, str):
-                            data[iCombStr][key] = output[key]
-
-                except:
-                    print('... file missing')
-                    missing = missing + 1
-                    output = {}
-            else:
-                missing = missing + 1
-
-        print('%d files missing' % (missing))
-
-        # save
-        if saveAll:
-            print('Saving to single file with all data')
-            filename = '%s/%s_allData.json' % (dataFolder, batchLabel)
-            dataSave = {'params': params, 'data': data}
-            with open(filename, 'w') as fileObj:
-                json.dump(dataSave, fileObj)
-
-        return params, data
 
 #--------------------------------------------------------------------
 # Function to convert data to Pandas
@@ -130,30 +46,24 @@ def toPandas(params, data):
 #--------------------------------------------------------------------
 # get rmp and save --- 
 #--------------------------------------------------------------------
+    #df = pd.DataFrame(rows, columns=cols)
+    #df['simLabel'] = list(data.keys())
+    #df['V_soma'][0] #output is OrderedDict([('cell_0',[-61.0,etc....])]) - voltages
 def get_rmp()
     # df = pd.DataFrame(rows, columns=col
     # df['simLabel'] = list(data.keys())
-    #df.keys
-    #Index(['amp', 'cellnum', 'V_soma', 'avgRate', 'spkid', 'spkt', 't','simLabel'],dtype='object')
+    # df.keys>>>> index(['amp', 'cellnum', 'V_soma', 'avgRate', 'spkid', 'spkt', 't','simLabel'],dtype='object')
     for i in list(df.simLabel):
         temp_str = df.simLabel[i]
         delta_t = cfg.recordStep #0.1 ms
         v0 = df['V_soma'][i]
         v1 = df['V_soma'][i+1]
 
+
         
-def readPlot():
-    # SET BATCHLABEL AND LOC OF DATA HERE
-    dataFolder = 'data' #'amp_data' #'tauWeight_data'
-    batchLabel = '21june16a'#'amp' #'tauWeight'
-    params, data = readBatchData(dataFolder, batchLabel, loadAll=0, saveAll=1, vars=None, maxCombs=None)
-
-
-
 
 # Main code
 if __name__ == '__main__':
-    readPlot()
     get_rmp()
 
 
