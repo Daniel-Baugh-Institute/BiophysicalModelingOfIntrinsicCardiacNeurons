41e47d471c750d316cb7b422d153d702b719af8d
Jessica Feldman <jessfeld@gmail.com>
2021-06-17 14:28:16 -0400

    updating test_rmp

--- test_rmp.py
+++ test_rmp.py
@@ -1,19 +1,131 @@
 #purpose: to show rmp. Creates scatter plot w/ one point for the rmp for each of 115 cellnums
 #load json file to extract v and time
 #(y1-y0)/(x1-x0)
+
 import json
 import pandas as pd
-#import seaborn as sb
 import matplotlib.pyplot as plt
 import numpy as np
 from pylab import *
 from itertools import product
-from pprint import pprint
 from netpyne import specs
 from collections import OrderedDict
+#from pprint import pprint
+#import seaborn as sb <<< deprecated 
 
-filename = []
-
-
-with open(filename, 'r') as fileObj:
+#--------------------------------------------------------------------
+# Function to read batch data
+#--------------------------------------------------------------------
+def readBatchData(dataFolder, batchLabel, loadAll=False, saveAll=True, vars=None, maxCombs=None, listCombs=None):
+    # load from previously saved file with all data
+    if loadAll:
+        print('\nLoading single file with all data...')
+        filename = '%s/%s/%s_allData.json' % (dataFolder, batchLabel, batchLabel)
+        with open(filename, 'r') as fileObj:
             dataLoad = json.load(fileObj, object_pairs_hook=OrderedDict)
+        params = dataLoad['params']
+        data = dataLoad['data']
+        return params, data
+
+    if isinstance(listCombs, str):
+        filename = str(listCombs)
+        with open(filename, 'r') as fileObj:
+            dataLoad = json.load(fileObj)
+        listCombs = dataLoad['paramsMatch']
+
+    # read the batch file and cfg
+    batchFile = '%s/%s_batch.json' % (dataFolder, batchLabel)
+    with open(batchFile, 'r') as fileObj:
+        b = json.load(fileObj)['batch']
+
+    # read params labels and ranges
+    params = b['params']
+
+    # reorder so grouped params come first
+    preorder = [p for p in params if 'group' in p and p['group']]
+    for p in params:
+        if p not in preorder: preorder.append(p)
+    params = preorder
+
+    # read vars from all files - store in dict
+    if b['method'] == 'grid':
+        labelList, valuesList = list(zip(*[(p['label'], p['values']) for p in params]))
+        valueCombinations = product(*(valuesList))
+        indexCombinations = product(*[list(range(len(x))) for x in valuesList])
+        data = {}
+        print('Reading data...')
+        missing = 0
+        for i,(iComb, pComb) in enumerate(zip(indexCombinations, valueCombinations)):
+            if (not maxCombs or i<= maxCombs) and (not listCombs or list(pComb) in listCombs):
+                print(i, iComb)
+                # read output file
+                iCombStr = ''.join([''.join('_'+str(i)) for i in iComb])
+                simLabel = b['batchLabel']+iCombStr
+                outFile = b['saveFolder']+'/'+simLabel+'.json'
+                try:
+                    with open(outFile, 'r') as fileObj:
+                        output = json.load(fileObj, object_pairs_hook=OrderedDict)
+                    # save output file in data dict
+                    data[iCombStr] = {}
+                    data[iCombStr]['paramValues'] = pComb  # store param values
+                    if not vars: vars = list(output.keys())
+
+                    for key in vars:
+                        if isinstance(key, tuple):
+                            container = output
+                            for ikey in range(len(key)-1):
+                                container = container[key[ikey]]
+                            data[iCombStr][key[1]] = container[key[-1]]
+
+                        elif isinstance(key, str):
+                            data[iCombStr][key] = output[key]
+
+                except:
+                    print('... file missing')
+                    missing = missing + 1
+                    output = {}
+            else:
+                missing = missing + 1
+
+        print('%d files missing' % (missing))
+
+        # save
+        if saveAll:
+            print('Saving to single file with all data')
+            filename = '%s/%s_allData.json' % (dataFolder, batchLabel)
+            dataSave = {'params': params, 'data': data}
+            with open(filename, 'w') as fileObj:
+                json.dump(dataSave, fileObj)
+
+        return params, data
+
+#--------------------------------------------------------------------
+# Function to convert data to Pandas
+#--------------------------------------------------------------------
+def toPandas(params, data):
+    if 'simData' in data[list(data.keys())[0]]:
+        rows = [list(d['paramValues'])+[s for s in list(d['simData'].values())] for d in list(data.values())]
+        cols = [str(d['label']) for d in params]+[s for s in list(data[list(data.keys())[0]]['simData'].keys())]
+    else:
+        rows = [list(d['paramValues'])+[s for s in list(d.values())] for d in list(data.values())]
+        cols = [str(d['label']) for d in params]+[s for s in list(data[list(data.keys())[0]].keys())]
+
+    df = pd.DataFrame(rows, columns=cols)
+    df['simLabel'] = list(data.keys())
+
+    colRename=[]
+    for col in list(df.columns):
+        if col.startswith("[u'"):
+            colName = col.replace(", u'","_'").replace("[u","").replace("'","").replace("]","").replace(", ","_")
+            colRename.append(colName)
+        else:
+            colRename.append(col)
+    #print(colRename)
+    df.columns = colRename
+
+    return df
+
+
+#### PLOT FUNCTIONALITY HERE (adapt from plot2Drate in analysis.py)
+
+
