4b8531e699dc45e7d9dfd5a94b41ad725724ef83
Jessica Feldman <jessfeld@gmail.com>
2021-06-18 14:59:06 -0400

    fixing ion issues in init_jf.py

--- init_jf.py
+++ init_jf.py
@@ -13,8 +13,8 @@ sim.create(netParams=netParams, simConfig=simConfig)
 def fi():
     '''set steady state RMP for 1 cell'''
     seg = sim.net.cells[0].secs.soma.hObj(0.5) # since only 1 cell with nseg=1 can jump straight to that seg
-    isum = seg.ina if h.ismembrane('na_ion') else 0 + seg.ik if h.ismembrane('k_ion') else 0 + seg.ica \
-        if h.ismembrane('ca_ion') else 0 + seg.iother if h.ismembrane('other_ion') else 0
+    isum = 0
+    isum = (seg.ina if h.ismembrane('na_ion') else 0) + (seg.ik if h.ismembrane('k_ion') else 0) + (seg.ica if h.ismembrane('ca_ion') else 0) + (seg.iother if h.ismembrane('other_ion') else 0)
     seg.e_pas = cfg.hParams['v_init']+isum/seg.g_pas
     print('AAAA: isum = ',isum, 'ipas = ',seg.i_pas, 'isum+ipas =',isum+seg.i_pas, 'epas = ',seg.e_pas,'v = ', seg.v)
 
