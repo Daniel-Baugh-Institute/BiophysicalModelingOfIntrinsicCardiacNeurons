73991ffba12704af79af6d3b623843aeabbbb2c3
Bill Lytton <wwlytton@yahoo.com>
2021-06-18 11:35:36 -0400

    get rid of comments and extraneous prints

--- init_jf.py
+++ init_jf.py
@@ -10,14 +10,10 @@ sim.create(netParams=netParams, simConfig=simConfig)
 
 def fi():
     '''set steady state RMP for 1 cell'''
-    print('AAAAAAAAAAAAAAAAAAAAAA')
     seg = sim.net.cells[0].secs.soma.hObj(0.5) # since only 1 cell with nseg=1 can jump straight to that seg
-    print('epas at beginning of fi() = ', seg.e_pas)
     isum = seg.ina + seg.ik + seg.ica + seg.iother
     seg.e_pas = cfg.hParams['v_init']+isum/seg.g_pas
-    #seg.g_pas = isum/(seg.e_pas-cfg.hParams['v_init'])
-    print('isum = ',isum, 'ipas = ',seg.i_pas, 'isum+ipas =',isum+seg.i_pas, 'epas at end of fi () = ',seg.e_pas,'v = ', seg.v)
-    #print(h.t)
+    print('AAAA: isum = ',isum, 'ipas = ',seg.i_pas, 'isum+ipas =',isum+seg.i_pas, 'epas = ',seg.e_pas,'v = ', seg.v)
 
 fih = [h.FInitializeHandler(2, fi)]
 sim.simulate()
