33aa9663f1a31295f50adf62ca7411c71ad0e147
Jessica Feldman <jessfeld@gmail.com>
2021-06-19 17:35:30 -0400

    modifying cfg file for epas

--- cfg.py
+++ cfg.py
@@ -1,5 +1,7 @@
 from netpyne import specs
+from netpyne.specs import simConfig
 cfg = specs.SimConfig() 
+#cfg = simConfig()
 
 cfg.hParams = {'celsius':35, 'v_init':-61}
 cfg.duration = 5 #500 # 1*1e3
@@ -7,6 +9,18 @@ cfg.dt = 0.025
 cfg.verbose = False     
 cfg.recordCells = ['all']
 cfg.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}} #var':'epas'}}
+# Recording/plotting parameters
+#simConfig.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'},
+#                          'ik_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'ik'},
+#                          'cai_soma': {'sec': 'soma', 'loc':0.5, 'var': 'cai'},
+#                          'cao_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'cao'}}
+cfg.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'},
+                    'V_soma':{'sec': 'soma', 'loc': 0.5,'var': 'epas'},
+                          'ik_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'ik'},
+                          'cai_soma': {'sec': 'soma', 'loc':0.5, 'var': 'cai'},
+                          'cao_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'cao'}}
+
+
 cfg.recordStep = 0.1       
 cfg.filename = 'test2'
 cfg.saveJson = True
