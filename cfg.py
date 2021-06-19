36dc02a803f49e948137815d36990dc747f2e029
Jessica Feldman <jessfeld@gmail.com>
2021-06-19 17:44:30 -0400

    updating cfg.py

--- cfg.py
+++ cfg.py
@@ -8,27 +8,27 @@ cfg.duration = 5 #500 # 1*1e3
 cfg.dt = 0.025          
 cfg.verbose = False     
 cfg.recordCells = ['all']
-cfg.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}} #var':'epas'}}
+#cfg.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}} #var':'epas'}}
 # Recording/plotting parameters
 #simConfig.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'},
 #                          'ik_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'ik'},
 #                          'cai_soma': {'sec': 'soma', 'loc':0.5, 'var': 'cai'},
 #                          'cao_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'cao'}}
 cfg.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'},
-                    'V_soma':{'sec': 'soma', 'loc': 0.5,'var': 'epas'},
                           'ik_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'ik'},
                           'cai_soma': {'sec': 'soma', 'loc':0.5, 'var': 'cai'},
-                          'cao_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'cao'}}
+                          'cao_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'cao'},
+                          'epas' : {'sec': 'soma', 'loc': 0.5,'var': 'e_pas'}}
 
 
 cfg.recordStep = 0.1       
-cfg.filename = 'test2'
+cfg.filename = 'test_epas'
 cfg.saveJson = True
 cfg.analysis['plotTraces'] = {'include': [0], 'saveFig': True}
 cfg.analysis['plotRaster'] = {'saveFig': True}                   # Plot a raster
 
-cfg.saveDataInclude = ['simData', 'simConfig', 'netParams', 'net', 'epas']
+cfg.saveDataInclude = ['simData', 'simConfig', 'netParams', 'net']
 
 cfg.stim = 'IClamp'
 cfg.amp= 0	#1
-cfg.cellnum = 0 #105
+cfg.cellnum = 114 #105
