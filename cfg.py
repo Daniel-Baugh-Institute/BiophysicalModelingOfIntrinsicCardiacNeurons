58ac8123b938e3486e42d202567c402618fe457b
Jessica Feldman <jessfeld@gmail.com>
2021-06-19 18:05:29 -0400

    added epas to cfg.py and changed batchLabel in batch.py

--- cfg.py
+++ cfg.py
@@ -10,10 +10,6 @@ cfg.verbose = False
 cfg.recordCells = ['all']
 #cfg.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}} #var':'epas'}}
 # Recording/plotting parameters
-#simConfig.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'},
-#                          'ik_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'ik'},
-#                          'cai_soma': {'sec': 'soma', 'loc':0.5, 'var': 'cai'},
-#                          'cao_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'cao'}}
 cfg.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'},
                           'ik_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'ik'},
                           'cai_soma': {'sec': 'soma', 'loc':0.5, 'var': 'cai'},
