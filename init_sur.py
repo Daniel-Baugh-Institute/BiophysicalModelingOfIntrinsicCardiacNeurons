744265eb0b416bc5f35eabbe90f5b11a85c9cbd5
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-06-21 16:51:48 -0400

    moved seg to outside fi()

--- init_sur.py
+++ init_sur.py
@@ -1,4 +1,4 @@
-from imp import IMP_HOOK
+# from imp import IMP_HOOK
 from future.standard_library import install_aliases
 from netpyne import sim
 from neuron import h
@@ -6,10 +6,10 @@ from neuron import h
 simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams.py')
 # sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)
 sim.create(netParams=netParams, simConfig=simConfig)
-
+seg = sim.net.cells[0].secs.soma.hObj(0.5)
 def fi():
     '''set steady state RMP for 1 cell'''
-    seg = sim.net.cells[0].secs.soma.hObj(0.5) # since only 1 cell with nseg=1 can jump straight to that seg
+     # since only 1 cell with nseg=1 can jump straight to that seg
     isum = 0
     isum = (seg.ina if h.ismembrane('na_ion') else 0) + (seg.ik if h.ismembrane('k_ion') else 0) + (seg.ica if h.ismembrane('ca_ion') else 0) + (seg.iother if h.ismembrane('other_ion') else 0)
     seg.e_pas = cfg.hParams['v_init']+isum/seg.g_pas
