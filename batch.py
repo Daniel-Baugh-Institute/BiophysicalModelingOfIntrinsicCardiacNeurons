de74c8349bb6535a67f0d94d9dbcb6d7bbbe0308
Jessica Feldman <jessfeld@gmail.com>
2021-06-25 11:09:47 -0400

    updating sim settings batch, netParams, test_epas_jf

--- batch.py
+++ batch.py
@@ -9,7 +9,7 @@ def batch():
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams.py')
         # Set output folder, grid method (all param combinations), and run configuration
         b.batchLabel = '21june25ajf' if sys.argv[-1]=='batch.py' else sys.argv[-1]
-        b.saveFolder = '21june25ajf'                #'data'
+        b.saveFolder = 'data'                #'data'
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
         b.run()
--- netParams.py
+++ netParams.py
@@ -27,5 +27,5 @@ netParams.cellParams['CEL'] = CEL
 netParams.popParams['U'] = {'cellType': 'CEL', 'numCells': 1}
 
 if cfg.stim == 'IClamp':
-    netParams.stimSourceParams['iclamp'] = {'type': 'IClamp', 'amp': cfg.amp, 'dur': 5, 'delay': 0} 
+    netParams.stimSourceParams['iclamp'] = {'type': 'IClamp', 'amp': cfg.amp, 'dur': 1e9, 'delay': 0} 
     netParams.stimTargetParams['iclamp->CEL'] = {'source': 'iclamp', 'conds': {'cellType': 'CEL'}, 'sec': 'soma', 'loc': 0.5}
