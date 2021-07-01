859ca691cb7f454eb63126d91cc7ddb3f9f7f374
Bill Lytton <wwlytton@yahoo.com>
2021-07-01 11:18:20 -0400

    stimend from params

--- test_sur.py
+++ test_sur.py
@@ -1,15 +1,17 @@
 def RinStats(df=df):
-	dfrin=df[['amp','cellnum']].copy()
-	dfrin['Vlist'] = df.V_soma.apply(lambda x: x['cell_0'])
- 	dfrin['Vmin'] = dfrin.Vlist.apply(min)
- 	# dfrin['indxss']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<800)))
- 	dfrin['indxss']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<data[list(data.keys())[c]]['net']['cells'][0]['stims'][0]['dur'])))
- 	isslist = dfrin.indxss.tolist() 
-	dfrin['vss']= dfrin.Vlist.apply(lambda x: x[i] for in isslist) #generator obj not callable
- 	#LATER
- 	# dfrin['indxrmp']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<50)))
- 	dfrin['indxrmp']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<data[list(data.keys())[c]]['net']['cells'][0]['stims'][0]['delay'])))
- 	return dfrin
+    stim = data[list(data)[0]]['net']['params']['stimSourceParams']['iclamp']
+    stimend = stim['dur'] + stim['delay']
+    dfrin=df[['amp','cellnum']].copy()
+    dfrin['Vlist'] = df.V_soma.apply(lambda x: x['cell_0'])
+    dfrin['Vmin'] = dfrin.Vlist.apply(min)
+    # dfrin['indxss']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<800)))
+    dfrin['indxss']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<data[list(data.keys())[c]]['net']['cells'][0]['stims'][0]['dur'])))
+    isslist = dfrin.indxss.tolist() 
+    dfrin['vss']= dfrin.Vlist.apply(lambda x: x[i] for in isslist) #generator obj not callable
+    #LATER
+    # dfrin['indxrmp']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<50)))
+    dfrin['indxrmp']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<data[list(data.keys())[c]]['net']['cells'][0]['stims'][0]['delay'])))
+    return dfrin
 
 # fig, axs = plt.subplots(2,1)
 # axs[0].scatter(list(r_vmin),list(r_vmin.values()))
