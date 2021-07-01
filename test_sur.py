9feff55c7c0400cd6f43844603e5a254a46cb7c0
Bill Lytton <wwlytton@yahoo.com>
2021-07-01 11:21:04 -0400

    get rid of nonfunctioning code and commends

--- test_sur.py
+++ test_sur.py
@@ -4,13 +4,7 @@ def RinStats(df=df):
     dfrin=df[['amp','cellnum']].copy()
     dfrin['Vlist'] = df.V_soma.apply(lambda x: x['cell_0'])
     dfrin['Vmin'] = dfrin.Vlist.apply(min)
-    # dfrin['indxss']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<800)))
-    dfrin['indxss']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<data[list(data.keys())[c]]['net']['cells'][0]['stims'][0]['dur'])))
-    isslist = dfrin.indxss.tolist() 
-    dfrin['vss']= dfrin.Vlist.apply(lambda x: x[i] for in isslist) #generator obj not callable
-    #LATER
-    # dfrin['indxrmp']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<50)))
-    dfrin['indxrmp']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<data[list(data.keys())[c]]['net']['cells'][0]['stims'][0]['delay'])))
+    dfrin['indxss']=df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<stimend)
     return dfrin
 
 # fig, axs = plt.subplots(2,1)
