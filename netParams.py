b269f94283c5132642fdf5a14434c1a24ef13278
Jessica Feldman <jessfeld@gmail.com>
2021-07-04 17:00:06 -0400

    update batch and netParams for 21july04a sim

--- netParams.py
+++ netParams.py
@@ -37,5 +37,5 @@ netParams.cellParams['CEL'] = CEL
 netParams.popParams['U'] = {'cellType': 'CEL', 'numCells': 1}
 
 if cfg.stim == 'IClamp':
-    netParams.stimSourceParams['iclamp'] = {'type': 'IClamp', 'amp': cfg.amp, 'dur': 300, 'delay': 100} 
+    netParams.stimSourceParams['iclamp'] = {'type': 'IClamp', 'amp': cfg.amp, 'dur': 300, 'delay': 50} 
     netParams.stimTargetParams['iclamp->CEL'] = {'source': 'iclamp', 'conds': {'cellType': 'CEL'}, 'sec': 'soma', 'loc': 0.5}
