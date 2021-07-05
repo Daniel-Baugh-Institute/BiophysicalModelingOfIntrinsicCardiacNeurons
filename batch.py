46ea19a72ef37760e15ffb19fe9c492fb4907188
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-05 14:52:47 -0400

    -ve amp, changed label, saveFolder

--- batch.py
+++ batch.py
@@ -4,12 +4,12 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        params['amp'] =  [0.3, 0.8, 1.0] # change to 0.3 0.7/0.8 1.0
+        params['amp'] = [-0.001]                   #[0.6, 0.8, 1.5] 
         params['cellnum'] = [x for x in range(115)]
-        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams.py')
+        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_sur.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21jul04d' #if sys.argv[-1]=='batch.py' else sys.argv[-1]
-        b.saveFolder = '/tera/jessica/data' #'data'        /tera/jessica/data        #'data'
+        b.batchLabel = '21jul05a' #if sys.argv[-1]=='batch.py' else sys.argv[-1]
+        b.saveFolder = '/tera/suri/'+b.batchLabel #'data'        /tera/jessica/data        #'data'
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
         b.run()
