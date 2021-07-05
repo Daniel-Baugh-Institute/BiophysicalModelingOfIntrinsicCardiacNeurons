462221046de47f0e4c10313668ce4093b1921b13
Jessica Feldman <jessfeld@gmail.com>
2021-07-04 20:48:10 -0400

    updated batch for new sim, added changes to plotspikeStats, created plot_AP from df

--- plotspikeStats.py
+++ plotspikeStats.py
@@ -7,10 +7,10 @@ from collections import OrderedDict
 from itertools import product
 import matplotlib.pyplot as plt
 
-batchLabel = '21jul04a'
+batchLabel = '21jul04b'
 filenamepkl = batchLabel+ '_spkStats.pkl'
 
-dir = '/u/jessica/ragp' #cd dir
+dir = '/u/jessica/ragp/ragp' #cd dir
 df=pd.read_pickle(filenamepkl)
 
 def ldSpikeStats(f=filenamepkl): return pd.read_pickle(f) 
@@ -40,15 +40,6 @@ def plt_ss():
     plt.savefig(batchLabel + '_spk1.png')
     plt.show()
 
-    for x in range(115): 
-        fig, ax = plt.subplots()
-        ax.clear()
-        df.loc[df.cellnum ==x].plot.line('V', 't', color=k, ax=ax)
-
-        #for a,c in zip(set(df['amp']), ('b','g','r')): df.loc[df.amp==a].plot.scatter('cellnum', 'spk1', color=c, ax=ax)
-    plt.title('Time of 1st spike per cellnum')
-    plt.savefig(batchLabel + '_spk1.png')
-    plt.show()
 
     
 
