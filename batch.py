aaed61156e2e6ce808891c68f5125b0ad94dbd1e
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-06 11:32:21 -0400

    Running batch sim to create Para Matrix (Raj)

--- batch.py
+++ batch.py
@@ -4,12 +4,12 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        params['amp'] = [-0.001, -0.003]                   #[0.6, 0.8, 1.5] 
+        params['amp'] = [0.6, 0.8, 1.0, 1.2, 1.4] #[-0.001, -0.002, -0.003,-0.004,-0.005]                   #[0.6, 0.8, 1.5] 
         params['cellnum'] = [x for x in range(115)]
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_sur.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21jul05a' #if sys.argv[-1]=='batch.py' else sys.argv[-1]
-        b.saveFolder = '/tera/suri/'+b.batchLabel #'data'        /tera/jessica/data        #'data'
+        b.batchLabel = '21jul06a' #if sys.argv[-1]=='batch.py' else sys.argv[-1]
+        b.saveFolder = b.batchLabel #'/tera/suri/'+b.batchLabel #'data'        /tera/jessica/data        #'data'
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
         b.run()
