426045486e7e4fec90473be2d3149723926bab7d
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-06 13:19:53 -0400

    varying -ve amp to get -80 mV hyperpoln

--- cfg.py
+++ cfg.py
@@ -25,11 +25,11 @@ cfg.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'}}
                     # 'ihcn4':{'sec': 'soma', 'loc': 0.5,'var': 'ihcn4_ch_Hcn4_cp12'}}
 
 cfg.stim = 'IClamp'
-cfg.amp= -0.001     
-cfg.cellnum = 0 
+cfg.amp= -0.005     
+cfg.cellnum = 6 
 
 cfg.recordStep = 0.1       
-cfg.filename = '21jul5a/'+str(cfg.cellnum)+str(cfg.amp)
+cfg.filename = '21jul6b/'+str(cfg.cellnum)+str(cfg.amp)
 cfg.saveJson = True
 cfg.analysis['plotTraces'] = {'include': [0], 'saveFig': True}
 cfg.analysis['plotRaster'] = {'saveFig': False}         
