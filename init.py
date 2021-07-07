573ba219ae200896083784360701caca7b1d4645
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-07 10:59:27 -0400

    changed label for netParams_A

--- init.py
+++ init.py
@@ -24,7 +24,7 @@ fih = [h.FInitializeHandler(2, fi)]
 sim.simulate()
 sim.analyze()
 
-file = open('edump_21jul07b.csv','a') 
+file = open('edump_21jul07A.csv','a') 
 csvwriter = csv.writer(file, delimiter = '\t')
 csvwriter.writerow(elist)
 file.close()
