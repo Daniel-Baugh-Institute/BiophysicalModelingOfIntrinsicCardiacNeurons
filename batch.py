620d8e10b0ab10b850daa8fa4ed590cf228f890e
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-08 12:50:00 -0400

    batch with amp = 0.3 to 1.2

--- batch.py
+++ batch.py
@@ -4,11 +4,11 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        params['amp'] = [1]                                     #[0.6, 0.8, 1.0, 1.2, 1.4] #[-0.001, -0.002, -0.003,-0.004,-0.005]              
+        params['amp'] = [0.3, 0.5, 0.8, 1, 1.2]                                    
         params['cellnum'] = [x for x in range(115)]
-        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_.py')
+        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21jul07'                               #if sys.argv[-1]=='batch.py' else sys.argv[-1]
+        b.batchLabel = '21jul08a'                               #if sys.argv[-1]=='batch.py' else sys.argv[-1]
         b.saveFolder = b.batchLabel                             #'/tera/suri/'+b.batchLabel        /tera/jessica/data   #'data'
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
