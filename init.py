0ce464a3e0fca1a0cfa3e6835b48310ce146ffda
Suranjana Gupta gupta8suranjana@gmail.com <suri@my.neurosim.downstate.edu>
2021-07-08 14:08:44 -0400

    changed labels to _D

--- init.py
+++ init.py
@@ -24,7 +24,7 @@ fih = [h.FInitializeHandler(2, fi)]
 sim.simulate()
 sim.analyze()
 
-file = open('edump_21jul08b.csv','a') 
+file = open('edump_21jul08c.csv','a') 
 csvwriter = csv.writer(file, delimiter = '\t')
 csvwriter.writerow(elist)
 file.close()
