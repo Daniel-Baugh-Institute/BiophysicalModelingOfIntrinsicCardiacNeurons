3f5af1eb82be50ca8793a8015d4849d3c43fcb66
Suranjana Gupta gupta8suranjana@gmail.com <suri@my.neurosim.downstate.edu>
2021-07-08 13:30:35 -0400

    changed labels, etc for _bespoke

--- batch.py
+++ batch.py
@@ -6,9 +6,9 @@ def batch():
         params = specs.ODict()
         params['amp'] = [0.3, 0.5, 0.8, 1, 1.2]                                    
         params['cellnum'] = [x for x in range(115)]
-        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
+        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_bespoke.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21jul08a'                               #if sys.argv[-1]=='batch.py' else sys.argv[-1]
+        b.batchLabel = '21jul08b'                               #if sys.argv[-1]=='batch.py' else sys.argv[-1]
         b.saveFolder = b.batchLabel                             #'/tera/suri/'+b.batchLabel        /tera/jessica/data   #'data'
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
