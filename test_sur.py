436db80448cc6efbaa3bbc1e8409093d7a28d470
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-08 14:43:22 -0400

    added routine for classificatn; removed non-working code in analysis.py

--- test_sur.py
+++ test_sur.py
@@ -1,12 +1,44 @@
-def idDepnBlock(df=df):
-    dblk['Vlist'] = df.V_soma.apply(lambda x: x['cell_0'])
-    dblk['Vend'] = dblk.apply(lambda row: row['Vlist'][4000:6000], axis =1)
-    dblk['Vdiff'] = dblk.Vend.apply(lambda x: max(x)-min(x) if min(x)+61>30 else -1)
+# Run spkStats() first
+def classifyAP(df=df):
+    stim = data[list(data)[0]]['net']['params']['stimSourceParams']['iclamp']
+    stimend = stim['dur'] + stim['delay']
+    db=df[['amp','cellnum']].copy()
+    db['Vlist'] = df.V_soma.apply(lambda x: x['cell_0'])
+    db['Vrmp'] = db.Vlist.apply(lambda x: x[0])
+
+    # db
+    db['ind1'] = df.t.apply(lambda x:len(np.flatnonzero((stimend-10)<np.array(x))))
+    db['ind2'] = df.t.apply(lambda x:len(np.flatnonzero(np.array(x)<stimend)))
+    db['Vend'] = db.apply(lambda row: row['Vlist'][row['ind1']:row['ind2']], axis =1)
+    db['Vdb'] = db.apply(lambda row: ? if row['Vend'].max()-row['Vend'].min()<1 and row['Vend'].min()-row['Vrmp'][0]<30 else -1)
+
+    # subthreshold -  No Na
+    db['Vsubth'] = df.V_soma.apply(lambda x: ? if max(x)<0 else -1)
+
+    # phasic - 1 AP
+    db['Vph'] = dfss.scnt.apply(lambda x: ? if x<=3 else -1)
+
+    # tonic - w/o sp
+    db['Vton']
+
+    # tonic - w/ sustained sp
+    db['Vton_infsp']
+
+    # tonic - w/o sustained sp
+    db['Vton_finsp']
+
     return
 
-#Using df graphics
-dfss.loc[df.amp==0.6].plot.scatter('cellnum', 'Vdiff')
-plt.show()
+
+# def idDepnBlock(df=df):
+#     dblk['Vlist'] = df.V_soma.apply(lambda x: x['cell_0'])
+#     dblk['Vend'] = dblk.apply(lambda row: row['Vlist'][4000:6000], axis =1)
+#     dblk['Vdiff'] = dblk.Vend.apply(lambda x: max(x)-min(x) if min(x)+61>30 else -1)
+#     return
+
+# #Using df graphics
+# dfss.loc[df.amp==0.6].plot.scatter('cellnum', 'Vdiff')
+# plt.show()
 
 # fig, ax = plt.subplots()
 # ax.clear()
