705d54cbf5e1abd441a8626ce78b9215cdf7a1b2
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-14 16:08:34 -0400

    reduced gkcnc to decrease ff

--- cfg.py
+++ cfg.py
@@ -29,7 +29,7 @@ cfg.amp= 0.1
 cfg.cellnum = 1 
 
 cfg.recordStep = 0.1       
-cfg.filename = '21jul14/'+'f'#+str(cfg.cellnum)+str(cfg.amp)
+cfg.filename = '21jul14/'+'g'#+str(cfg.cellnum)+str(cfg.amp)
 cfg.saveJson = True
 cfg.analysis['plotTraces'] = {'include': [0], 'saveFig': True}
 cfg.analysis['plotRaster'] = {'saveFig': False}         
