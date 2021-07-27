05b306808d630ba21ca6858d9beaa889eace4aee
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-27 12:28:28 -0400

    Commented the cadad component of rmp() code

--- init.py
+++ init.py
@@ -7,24 +7,16 @@ simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsD
 sim.create(netParams=netParams, simConfig=simConfig)
 
 seg = sim.net.cells[0].secs.soma.hObj(0.5) # since only 1 cell with nseg=1 can jump straight to that seg
-# elist = []
+elist = []
 
 def fi():
     '''set steady state RMP for 1 cell'''
     isum = 0
     isum = (seg.ina if h.ismembrane('na_ion') else 0) + (seg.ik if h.ismembrane('k_ion') else 0) + (seg.ica if h.ismembrane('ca_ion') else 0) + (seg.iother if h.ismembrane('other_ion') else 0)
     seg.e_pas = cfg.hParams['v_init']+isum/seg.g_pas 
-    if h.ismembrane("me_cadad_Adestexhe"):
-        h.setdata_me_cadad_Adestexhe(seg(0.5))
-        seg.kd_me_cadad_Adestexhe = seg.cai*(-(seg.cai * seg.depth_me_cadad_Adestexhe *h.FARADAY) + seg.cainf_me_cadad_Adestexhe * seg.depth_me_cadad_Adestexhe * h.FARADAY - \
-            5000*seg.ica*seg.taur_me_cadad_Adestexhe - seg.depth_me_cadad_Adestexhe * h.FARADAY*seg.kt_me_cadad_Adestexhe * seg.taur_me_cadad_Adestexhe)
-        seg.kd_me_cadad_Adestexhe = seg.kd_me_cadad_Adestexhe/(seg.cai*seg.depth_me_cadad_Adestexhe*h.FARADAY - seg.cainf_me_cadad_Adestexhe * seg.depth_me_cadad_Adestexhe * h.FARADAY + \
-               5000*seg.ica*seg.taur_me_cadad_Adestexhe)
-    else:
-        0
     print(cfg.cellnum)
     print(seg.e_pas)
-    # elist.append(seg.e_pas)
+    elist.append(seg.e_pas)
 
 
 
@@ -32,7 +24,15 @@ fih = [h.FInitializeHandler(2, fi)]
 sim.simulate()
 sim.analyze()
 
-# file = open('edump_21jul26b.csv','a') 
-# csvwriter = csv.writer(file, delimiter = '\t')
-# csvwriter.writerow(elist)
-# file.close()
+file = open('edump_21jul26b.csv','a') 
+csvwriter = csv.writer(file, delimiter = '\t')
+csvwriter.writerow(elist)
+file.close()
+
+# if h.ismembrane("me_cadad_Adestexhe"):
+#         h.setdata_me_cadad_Adestexhe(seg(0.5))
+#         seg.kd_me_cadad_Adestexhe = seg.cai*(-(seg.cai * seg.depth_me_cadad_Adestexhe *h.FARADAY) + seg.cainf_me_cadad_Adestexhe * seg.depth_me_cadad_Adestexhe * h.FARADAY - \
+#             5000*seg.ica*seg.taur_me_cadad_Adestexhe - seg.depth_me_cadad_Adestexhe * h.FARADAY*seg.kt_me_cadad_Adestexhe * seg.taur_me_cadad_Adestexhe)
+#         seg.kd_me_cadad_Adestexhe = seg.kd_me_cadad_Adestexhe/(seg.cai*seg.depth_me_cadad_Adestexhe*h.FARADAY - seg.cainf_me_cadad_Adestexhe * seg.depth_me_cadad_Adestexhe * h.FARADAY + \
+#                5000*seg.ica*seg.taur_me_cadad_Adestexhe)
+
