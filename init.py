ba048fd502bf012260b2ad9bbe9f574a7ea715c9
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-27 05:09:00 -0400

    Added code to stabilise rmp

--- init.py
+++ init.py
@@ -14,17 +14,24 @@ def fi():
     isum = 0
     isum = (seg.ina if h.ismembrane('na_ion') else 0) + (seg.ik if h.ismembrane('k_ion') else 0) + (seg.ica if h.ismembrane('ca_ion') else 0) + (seg.iother if h.ismembrane('other_ion') else 0)
     seg.e_pas = cfg.hParams['v_init']+isum/seg.g_pas
+    if h.ismembrane("me_cadad_Adestexhe"):
+        kd_me_cadad_Adestexhe = cai*(-(cai * depth_me_cadad_Adestexhe *FARADAY) + cainf_me_cadad_Adestexhe * depthme_cadad_Adestexhe * FARADAY - \
+            5000*ica*taur_me_cadad_Adestexhe - depth_me_cadad_Adestexhe * FARADAY*kt_me_cadad_Adestexhe * taur_me_cadad_Adestexhe)
+        kd_me_cadad_Adestexhe = kd_me_cadad_Adestexhe/(cai*depth_me_cadad_Adestexhe*FARADAY - cainf_me_cadad_Adestexhe * depth_me_cadad_Adestexhe * FARADAY + \
+               5000*ica*taur_me_cadad_Adestexhe)
+    else:
+        0
     print(cfg.cellnum)
     print(seg.e_pas)
     elist.append(seg.e_pas)
 
-    
+
 
 fih = [h.FInitializeHandler(2, fi)]
 sim.simulate()
 sim.analyze()
 
-file = open('edump_21jul26a.csv','a') 
+file = open('edump_21jul26b.csv','a') 
 csvwriter = csv.writer(file, delimiter = '\t')
 csvwriter.writerow(elist)
 file.close()
