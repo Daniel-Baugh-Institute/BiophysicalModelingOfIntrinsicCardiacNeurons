94b345bf8e1ece49d4e7258f0f54fa9a31cb18b7
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-27 06:17:57 -0400

    removed KCa mechs since error as no network created

--- netParams_A.py
+++ netParams_A.py
@@ -19,10 +19,12 @@ cell = cell_identities[cfg.cellnum]
 
 ## Cell parameters/rules
 CEL = {'secs': {}}
-CEL['secs']['soma'] = {'geom': {'diam': 30, 'L': 30, 'Ra': 35.4, 'cm':1}, 'mechs':  {{'pas' : {'g': 1.8e-6, 'e': -65}}, 
-                                                                                    {'ch_Kcnn3_md169457': {'gSK_E2bar':0.000001}}, 
-                                                                                    {'ch_Kcnma1_md97860': {'gbar':1}},
-                                                                                    {'me_cadad_Adestexhe':{'depth':1, 'taur':5, 'cainf':2.4e-4}}}}
+CEL['secs']['soma'] = {'geom': {'diam': 30, 'L': 30, 'Ra': 35.4, 'cm':1}, 'mechs':  {{'pas' : {'g': 1.8e-6, 'e': -65}}}}
+
+# 
+#                                                                                     {'ch_Kcnn3_md169457': {'gSK_E2bar':0.000001}}, 
+#                                                                                     {'ch_Kcnma1_md97860': {'gbar':1}},
+#                                                                                     {'me_cadad_Adestexhe':{'depth':1, 'taur':5, 'cainf':2.4e-4
 for mod,onoff in zip(genemod,cell):
     if onoff:
         CEL['secs']['soma']['mechs'][mod]=genemod[mod]
