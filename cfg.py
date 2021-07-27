f20182528fea9f5a0cb043ef02b70ab8e258eae9
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-27 06:37:45 -0400

    fxd: filename pointed to folder; no json was created

--- cfg.py
+++ cfg.py
@@ -29,7 +29,7 @@ cfg.amp= 0
 cfg.cellnum = 1 
 
 cfg.recordStep = 0.1       
-cfg.filename = '21jul26b/'
+cfg.filename = '21jul26b/0'
 cfg.saveJson = True
 cfg.analysis['plotTraces'] = {'include': [0], 'saveFig': True}
 cfg.analysis['plotRaster'] = {'saveFig': False}         
