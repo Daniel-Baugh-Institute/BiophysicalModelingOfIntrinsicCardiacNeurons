95d460d8fa3998746583d1e857ed5944389b4485
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-27 06:24:48 -0400

    debugging why list index out of range: remvd elist

--- init.py
+++ init.py
@@ -7,13 +7,15 @@ simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsD
 sim.create(netParams=netParams, simConfig=simConfig)
 
 seg = sim.net.cells[0].secs.soma.hObj(0.5) # since only 1 cell with nseg=1 can jump straight to that seg
-elist = []
+# elist = []
 
 def fi():
     '''set steady state RMP for 1 cell'''
     isum = 0
     isum = (seg.ina if h.ismembrane('na_ion') else 0) + (seg.ik if h.ismembrane('k_ion') else 0) + (seg.ica if h.ismembrane('ca_ion') else 0) + (seg.iother if h.ismembrane('other_ion') else 0)
     seg.e_pas = cfg.hParams['v_init']+isum/seg.g_pas
+
+    
     # if h.ismembrane("me_cadad_Adestexhe"):
     #     kd_me_cadad_Adestexhe = cai*(-(cai * depth_me_cadad_Adestexhe *FARADAY) + cainf_me_cadad_Adestexhe * depthme_cadad_Adestexhe * FARADAY - \
     #         5000*ica*taur_me_cadad_Adestexhe - depth_me_cadad_Adestexhe * FARADAY*kt_me_cadad_Adestexhe * taur_me_cadad_Adestexhe)
@@ -21,9 +23,9 @@ def fi():
     #            5000*ica*taur_me_cadad_Adestexhe)
     # else:
     #     0
-    print(cfg.cellnum)
-    print(seg.e_pas)
-    elist.append(seg.e_pas)
+    # print(cfg.cellnum)
+    # print(seg.e_pas)
+    # elist.append(seg.e_pas)
 
 
 
@@ -31,7 +33,7 @@ fih = [h.FInitializeHandler(2, fi)]
 sim.simulate()
 sim.analyze()
 
-file = open('edump_21jul26b.csv','a') 
-csvwriter = csv.writer(file, delimiter = '\t')
-csvwriter.writerow(elist)
-file.close()
+# file = open('edump_21jul26b.csv','a') 
+# csvwriter = csv.writer(file, delimiter = '\t')
+# csvwriter.writerow(elist)
+# file.close()
