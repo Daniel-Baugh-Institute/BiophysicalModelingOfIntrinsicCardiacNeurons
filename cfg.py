a9f41ff4fdb25378889b30569483ab6181daff9f
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-27 01:27:27 -0400

    checking rmp: amp 0 for cell 1, changed label

--- cfg.py
+++ cfg.py
@@ -3,7 +3,7 @@ from netpyne.specs import simConfig
 cfg = specs.SimConfig() 
 
 cfg.hParams = {'celsius':35, 'v_init':-61}
-cfg.duration = 600  #1500 #1*1e3 #(ms)
+cfg.duration = 5  #1500 #1*1e3 #(ms)
 cfg.dt = 0.025          
 cfg.verbose = False     
 cfg.recordCells = ['all']
@@ -25,11 +25,11 @@ cfg.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'}}
                     # 'ihcn4':{'sec': 'soma', 'loc': 0.5,'var': 'ihcn4_ch_Hcn4_cp12'}}
 
 cfg.stim = 'IClamp'
-cfg.amp= 0.7     
-cfg.cellnum = 3 
+cfg.amp= 0     
+cfg.cellnum = 1 
 
 cfg.recordStep = 0.1       
-cfg.filename = '21jul26a/'+str(cfg.cellnum)+'_'+str(cfg.amp)+'_0.0054'
+cfg.filename = '21jul26b/'
 cfg.saveJson = True
 cfg.analysis['plotTraces'] = {'include': [0], 'saveFig': True}
 cfg.analysis['plotRaster'] = {'saveFig': False}         
