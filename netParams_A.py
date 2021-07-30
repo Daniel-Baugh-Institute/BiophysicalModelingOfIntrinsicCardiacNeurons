13c0d757d6ded180e0f27faee59fddfa1e1af4d0
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-30 13:41:12 -0400

    RESTORED Kcnab1 cond., zeroed SK and BK

--- netParams_A.py
+++ netParams_A.py
@@ -12,7 +12,7 @@ genemod = {'ch_Cacna1a_cp5':{'gCav2_1bar': 0.00001},  'ch_Cacna1b_cp6':{'gCav2_2
            'ch_Cacna1c_cp3':{'gLbar': 0.0001},        'ch_Cacna1g_cp41':{'gCav3_1bar': 0.00001},
            'ch_Cacna1i_md279':{'gcabar': 0.00027},  'ch_Hcn1_cp9':{'gHCN1bar': 0.00001},         #pcabar =.2e-3; 'gCav3_3bar': 0.0001
            'ch_Hcn2_cp10':{'gHCN2bar': 0.009},           'ch_Hcn3_cp11':{'gHCN3bar': 0.0001},    
-           'ch_Hcn4_cp12':{'gHCN4bar': 0.0002},           'ch_Kcna1ab1_md80769':{'gbar': 0.0054},   #0.015
+           'ch_Hcn4_cp12':{'gHCN4bar': 0.0002},           'ch_Kcna1ab1_md80769':{'gbar': 0.015},   #0.015; 0.0054
            'ch_Kcnc1_md74298':{'gk': 0.015},            'ch_Scn1a_md264834':{'gNav11bar': 1.0}}     #1.0
 cell_identities = np.bool_(np.transpose(np.genfromtxt('allcells_new12_unique_binary.csv', delimiter=',')))
 cell = cell_identities[cfg.cellnum]
@@ -20,8 +20,8 @@ cell = cell_identities[cfg.cellnum]
 ## Cell parameters/rules
 CEL = {'secs': {}}
 CEL['secs']['soma'] = {'geom': {'diam': 30, 'L': 30, 'Ra': 35.4, 'cm':1}, 'mechs':  {'pas' : {'g': 1.8e-6, 'e': -65},
-                                                                                    'ch_Kcnn3_md169457': {'gSK_E2bar':0.000001}, #0.000001
-                                                                                    'ch_Kcnma1_md97860': {'gbar':1}, #1
+                                                                                    'ch_Kcnn3_md169457': {'gSK_E2bar':0}, #0.000001
+                                                                                    'ch_Kcnma1_md97860': {'gbar':0}, #1
                                                                                     'cadad':{'depth':2, 'taur':5, 'cainf':2.4e-4} }   }
                                                                                 
 
