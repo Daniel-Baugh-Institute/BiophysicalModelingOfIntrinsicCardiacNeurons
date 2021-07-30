37ba3a780cacdbdb96d4c8e3ace158d900bfe5b3
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-07-30 13:59:38 -0400

    Chking active with gKca = 0

--- cfg.py
+++ cfg.py
@@ -3,7 +3,7 @@ from netpyne.specs import simConfig
 cfg = specs.SimConfig() 
 
 cfg.hParams = {'celsius':35, 'v_init':-61}
-cfg.duration = 10  #1500 #1*1e3 #(ms)
+cfg.duration = 600  #1500 #1*1e3 #(ms)
 cfg.dt = 0.025          
 cfg.verbose = False     
 cfg.recordCells = ['all']
@@ -26,11 +26,11 @@ cfg.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'},
                     # 'ihcn4':{'sec': 'soma', 'loc': 0.5,'var': 'ihcn4_ch_Hcn4_cp12'}}
 
 cfg.stim = 'IClamp'
-cfg.amp= 0    
+cfg.amp= 0.6    
 cfg.cellnum = 1 
 
 cfg.recordStep = 0.1       
-cfg.filename = '21jul30a/1'
+cfg.filename = '21jul30a/2'
 cfg.saveJson = True
 cfg.analysis['plotTraces'] = {'include': [0], 'saveFig': True}
 cfg.analysis['plotRaster'] = {'saveFig': False}         
