5f67c5c91067324065acff5c59e78e0b9f2c7761
Suranjana Gupta gupta8suranjana@gmail.com <suri@my.neurosim.downstate.edu>
2021-08-02 19:44:07 -0400

    Reduced #parameters for successful batch sim

--- batch.py
+++ batch.py
@@ -7,14 +7,14 @@ def batch():
         params = specs.ODict()
         params['amp'] = [0.6, 0.9]                                    
         params['cellnum'] = [x for x in range(115)]
-        params['gsk'] =  [0, 0.001, 0.003, 0.005, 0.008, 0.01, 0.03, 0.05, 0.08]
-        params['tau'] =  [x for x in np.arange(1,16,2)]
-        params['gkcna'] = [0.015, 0.03, 0.05, 0.08, 0.1]
-        params['gkcnc'] = [0.015, 0.03, 0.05, 0.08, 0.1]
-        params['depth'] = [x for x in np.arange(0.5,4,0.5)]
+        params['gsk'] =  [0, 0.001, 0.003, 0.005, 0.01, 0.05]
+        params['tau'] =  [x for x in np.arange(1,16,4)]
+        params['gkcna'] = [0.015, 0.03, 0.05, 0.08]
+        params['gkcnc'] = [0.015, 0.03, 0.05, 0.08]
+        params['depth'] = [x for x in range(3)]
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21aug02d'                               #if sys.argv[-1]=='batch.py' else sys.argv[-1]
+        b.batchLabel = '21aug02e'                               #if sys.argv[-1]=='batch.py' else sys.argv[-1]
         b.saveFolder = '/tera/suri/'+b.batchLabel                             #'/tera/suri/'+b.batchLabel        /tera/jessica/data   #'data'
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
