9b836d84d3c18ab85285da0a8f750b91b187af7c
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-08-02 12:40:40 -0400

    Added batch parameters,values; changed netParamsfile; label

--- batch.py
+++ batch.py
@@ -1,14 +1,19 @@
 import sys
+import numpy as np
 from netpyne import specs
 from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        params['amp'] = [0.3, 0.5, 0.8, 1, 1.2]                                    
-        params['cellnum'] = [x for x in range(115)]
-        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_D.py')
+        params['amp'] = [0.6]   #[0.6, 0.9]                                    
+        params['cellnum'] = [1]    #[x for x in range(115)]
+        params['gsk'] =  [0.001]       #[0, 0.001, 0.003, 0.005, 0.008, 0.01, 0.03, 0.05, 0.08]
+        params['tau'] =  [3, 5, 9]       #[x for x in np.arange(1,6,0.5)]
+        params['gkcna'] = [0.015]      #[0.015, 0.03, 0.05, 0.08, 0.1]
+        params['gkcnc'] = [0.015]             #[0.015, 0.03, 0.05, 0.08, 0.1]
+        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21jul08c'                               #if sys.argv[-1]=='batch.py' else sys.argv[-1]
+        b.batchLabel = '21aug02a'                               #if sys.argv[-1]=='batch.py' else sys.argv[-1]
         b.saveFolder = b.batchLabel                             #'/tera/suri/'+b.batchLabel        /tera/jessica/data   #'data'
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
