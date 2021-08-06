bfdeecc2f82970730baa2df5e5693abf85d88b31
Suranjana Gupta gupta8suranjana@gmail.com <suri@zn.neurosim.downstate.edu>
2021-08-06 17:59:31 -0400

    Testing Big Batch (8k sims); with pc.done() and h.quit() moved to end of grid.py

--- batch.py
+++ batch.py
@@ -10,12 +10,12 @@ def batch():
         #params['gsk'] =  [0, 0.001,0.003, 0.005, 0.01, 0.05]
         # params['tau'] =  [x for x in np.arange(1,16,4)]
         # params['depth'] = [x for x in range(3)]
-        params['gkcna'] = [0.015] #, 0.03, 0.05, 0.1, 0.3, 0.5]
-        params['gkcnc'] = [0.015, 0.03] #, 0.05, 0.1, 0.3, 0.5]
+        params['gkcna'] = [0.015, 0.03, 0.05, 0.1, 0.3, 0.5]
+        params['gkcnc'] = [0.015, 0.03, 0.05, 0.1, 0.3, 0.5]
         
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21aug06d' 
+        b.batchLabel = '21aug06e' 
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
