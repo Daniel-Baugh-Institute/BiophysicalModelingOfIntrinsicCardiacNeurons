5736b5c79931c4c7295ac86048090b724ee2740e
Suranjana Gupta gupta8suranjana@gmail.com <suri@pi.neurosim.downstate.edu>
2021-08-12 01:40:18 -0400

    Running Big Batch with 2 KDRs w/o LCM channels; increased tstop 1sec; dur, del; label

--- batch.py
+++ batch.py
@@ -7,15 +7,12 @@ def batch():
         params = specs.ODict()
         params['amp'] = [0.6, 1.0] #, 1.5]                                    
         params['cellnum'] = [x for x in range(115)]
-        #params['gsk'] =  [0, 0.001,0.003, 0.005, 0.01, 0.05]
-        # params['tau'] =  [x for x in np.arange(1,16,4)]
-        # params['depth'] = [x for x in range(3)]
         params['gkcna'] = [0.015, 0.03, 0.05, 0.1, 0.3, 0.5]
         params['gkcnc'] = [0.015, 0.03, 0.05, 0.1, 0.3, 0.5]
         
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21aug06e' 
+        b.batchLabel = '21aug12a' 
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
