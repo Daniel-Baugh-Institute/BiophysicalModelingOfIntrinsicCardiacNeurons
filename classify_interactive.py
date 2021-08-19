38ca72e41ea9ea2ee9bc651b131eb49ac6a3e878
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-08-19 13:04:15 -0400

    adhoc nbk for interactive testing of classification algo. (will merge into analysis.py later)

---
+++ classify_interactive.py
@@ -0,0 +1,29 @@
+stim = data[list(data)[0]]['net']['params']['stimSourceParams']['iclamp']
+stimend = stim['dur'] + stim['delay']
+dclass=df[['amp','cellnum']].copy()
+dclass['Vlist'] = df.V_soma.apply(lambda x: x['cell_0'])
+dclass['Vrmp'] = dclass.Vlist.apply(lambda x: x[0])
+dclass['spkend'] = df.spkt.apply(lambda x: x[len(x)-1] if len(x)>0 else -1)
+dfss=df[['amp', 'cellnum', 'avgRate']].copy() 
+dfss['scnt'] = df.spkt.apply(len)
+
+dclass['Vsubth'] = dclass.Vlist.apply(lambda x: 1 if max(x)<0 else 0)
+dclass['Vph'] = dfss.scnt.apply(lambda x: 1 if 0<x<=3 else 0)
+dclass['Vton'] = df.spkt.apply(lambda x: 1 if len(x)>3 and stim['delay']<=x[-1]<=stimend+5 else 0)
+dclass['Vton_susps'] = dclass.spkend.apply(lambda x: 1 if stimend+5<=x>=data[list(data)[0]]['simConfig']['duration']-50 else 0)
+dclass['Vton_brfps'] = dclass.spkend.apply(lambda x: 1 if stimend+5<=x<=data[list(data)[0]]['simConfig']['duration']-50 else 0)
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+ax.clear()
+for a,c in zip(set(df['amp']), ('g','r')): dclass.loc[df.amp==a].plot.scatter('cellnum', 'Vsubth', color=c, ax=ax)
+plt.title('Subthreshold')
+plt.savefig('21aug11a'+ '_0s.png')
+plt.show()
+
+fig, ax = plt.subplots()
+ax.clear()
+for a,c in zip(set(df['amp']), ('g','r')): dclass.loc[df.amp==a].plot.scatter('cellnum', 'Vph', color=c, ax=ax)
+plt.title('Phasic')
+plt.savefig('21aug11a'+ '_0p.png')
+plt.show()
