6cfb66f7c20c34c7cb32fac36e98cc904812e6c0
Suranjana Gupta gupta8suranjana@gmail.com <suri@pi.neurosim.downstate.edu>
2021-09-14 16:35:17 -0400

    Single batch run with mpi on my

--- batch.py
+++ batch.py
@@ -14,7 +14,7 @@ def batch():
 
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21sep14a' 
+        b.batchLabel = '21sep14b' 
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
