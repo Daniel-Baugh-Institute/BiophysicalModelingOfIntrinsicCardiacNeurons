8f661d36ba5f87acf23f1212a521c11df9173b4d
Suranjana Gupta gupta8suranjana@gmail.com <suri@my.neurosim.downstate.edu>
2021-09-16 10:13:45 -0400

    Running same batch with mpiexec - label changed

--- batch.py
+++ batch.py
@@ -14,7 +14,7 @@ def batch():
 
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21sep14g' 
+        b.batchLabel = '21sep14h' 
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
