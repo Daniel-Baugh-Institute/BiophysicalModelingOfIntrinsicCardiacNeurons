99e9e50a7b4c84ed650016bec54526fe88ceeb25
Suranjana Gupta gupta8suranjana@gmail.com <suri@my.neurosim.downstate.edu>
2021-09-16 10:02:48 -0400

    Placed cfg.simLabel ahead of cfg.saveFolder

--- cfg.py
+++ cfg.py
@@ -37,8 +37,8 @@ cfg.hcn4 = 0.0002
 
 cfg.recordStep = 0.1       
 # cfg.filename = '21sep14d/'
-cfg.saveFolder = '/tera/' + os.getlogin() + '/' +'21sep14e'
-cfg.simLabel = '21sep14e'
+cfg.simLabel = '21sep14f'
+cfg.saveFolder = '/tera/' + os.getlogin() + '/' +cfg.simLabel
 cfg.saveJson = True
 cfg.analysis['plotTraces'] = {'include': [0], 'saveFig': False}
 cfg.analysis['plotRaster'] = {'saveFig': False}         
