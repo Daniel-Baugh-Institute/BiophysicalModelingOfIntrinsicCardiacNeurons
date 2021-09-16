9d1f97311a26aa5b1335ec1299d6163094d6e800
Suranjana Gupta gupta8suranjana@gmail.com <suri@my.neurosim.downstate.edu>
2021-09-16 10:11:54 -0400

    2amp, 1cell, 1/hcn batch sim

--- batch.py
+++ batch.py
@@ -5,7 +5,7 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        params['amp'] = [0.6] #[0.6, 1.0]
+        params['amp'] = [0.6, 1.0]
         params['cellnum'] = [28] #[x for x in range(101)]
         params['hcn1'] = [0.00001] #[0.00001, 0.0001, 0.001, 0.01] 
         params['hcn2'] = [0.009] #[0.00009, 0.0009, 0.009, 0.09]
@@ -14,7 +14,7 @@ def batch():
 
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21sep14c' 
+        b.batchLabel = '21sep14g' 
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
