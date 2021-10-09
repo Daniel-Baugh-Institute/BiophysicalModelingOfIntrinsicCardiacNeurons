8b3af87428058097af6c4197a23ebac75dba7397
Bill Lytton <wwlytton@yahoo.com>
2021-10-09 10:31:27 -0400

    put runworker() into top level and get error

--- batch.py
+++ batch.py
@@ -6,6 +6,7 @@ import sys, inspect
 from netpyne.batch import Batch
 
 print('SOURCE: %s'%(inspect.getsourcefile(Batch)))
+pc = h.ParallelContext()
 
 def batch():
         params = specs.ODict()
@@ -13,13 +14,14 @@ def batch():
         params['cellnum'] = [x for x in range(4)] # 101
         # params['rybak'] = [0, 0.01, 0.03, 0.05, 0.07, 0.1, 0.12, 0.15] 
         params['ka'] = [0, 0.02] # , 0.04, 0.06, 0.08, 0.1, 0.12, 0.15] 
-
-        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
-        # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21oct08a' 
-        b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
-        b.method = 'grid'
-        b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
-        b.run()
+        pc.runworker()
+        
+#        b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
+#        # Set output folder, grid method (all param combinations), and run configuration
+#        b.batchLabel = '21oct08a' 
+#        b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
+#        b.method = 'grid'
+#        b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
+#        b.run()
 
 batch()
