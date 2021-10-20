a968827b0be3c2d5abba7042b0444a678d01e1d2
Bill Lytton <wwlytton@yahoo.com>
2021-10-20 15:28:21 -0400

    full history in sobol branch

--- sobol.py
+++ sobol.py
@@ -11,7 +11,7 @@ def parseBatchParams (b):
     bl, pl = [(i, p.match(l)) for i,l in enumerate(lines)], [] # bl: lines that match regexp
     for i,m in bl:
         if m:
-            try: 
+            try:
                 pl.append((m.group(1), eval(m.group(2)), m.group(3))) # strings: name, valueList, [indexed]
             except Exception as e:
                 print(f"ERROR >>>{e}<<<\n\tunable to evaluate '{m.group(2)}':\n\tline {i}: {m.string.strip()}")
@@ -21,7 +21,7 @@ def sobcall (pl, num, seed=33):
     ''' determine the min, max of sobolized params and do the combos with indexed params '''
     labels, Mins, Maxs, ilabels, ivals = [],[],[],[],[]
     for x in pl:
-        if (not x[2]):
+        if not x[2]:
             labels.append(x[0]); Mins.append(min(x[1])); Maxs.append(max(x[1]))
         else:
             ilabels.append(x[0]); ivals.append(x[1])
@@ -51,11 +51,10 @@ def getArgs ():
     # parser.add_argument("-r", default='sobol.csv', help='raw output from sobol call (default ./sobol.csv)')
     parser.add_argument("-f", default='params.csv', help='file for saving param lists (default ./params.csv)')
     parser.add_argument("-s", default=1234, type=int, help='seed')
+    parser.add_argument("-z", action='store_true', default=False, help='append a zero to each sobolized param (eg to turn off ion channel)')
     # parser.add_argument("-v", action='store_true', default=False, help='output to terminal')
     parser.add_argument("-b", default='batch.py', help='name of batchfile with "params" ranges (default ./batch.py)')
     return parser.parse_args()
-
-    np.savetxt(ag.f, allvals, delimiter=',', fmt='%.10f')
 
 if __name__ == '__main__':
     ag = getArgs()
