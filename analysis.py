a19a045794ba38740d470d465eab1bd4f5e5eb57
Suranjana Gupta gupta8suranjana@gmail.com <suri@pi.neurosim.downstate.edu>
2021-10-27 14:24:13 -0400

    changed params to labelList

--- analysis.py
+++ analysis.py
@@ -82,7 +82,7 @@ def readBatchData(dataFolder, batchLabel, target=None, saveAll=True, vars=None, 
         print('%d files missing' % (missing))
     elif b['method'] == 'list':
         dfpara = pd.read_csv('params.csv',delimiter=',')
-	params = dfpara.columns.values.tolist()
+	labelList = dfpara.columns.values.tolist()
 	#pass
 
     else:
