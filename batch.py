16514c499e70b2bc30827d08365ec0c2abd9ce0c
Suranjana Gupta gupta8suranjana@gmail.com <suri@pi.neurosim.downstate.edu>
2021-11-05 16:25:49 -0400

    Testing grid

--- batch.py
+++ batch.py
@@ -8,14 +8,14 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        params['amp'] = [0.3, 0.6, 0.9] # indexed
-        params['cellnum'] = [x for x in range(101)] # indexed
+        params['amp'] = [0.3] #, 0.6, 0.9] # indexed
+        params['cellnum'] = [0,1] #[x for x in range(101)] # indexed
         #params['rybak'] = [0, 0.01, 0.03, 0.05, 0.07, 0.1, 0.12, 0.15] 
-        params['ka'] = [0.08, 0.1, 0.12, 0.15] 
+        params['ka'] = [0] #[0.08, 0.1, 0.12, 0.15] 
 
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21oct25a' 
+        b.batchLabel = '21oct5m' 
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'list'  # params.csv will be read by default; else need name in cfg.paramListFile
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
