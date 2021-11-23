73d62da69ee80b8d4716a695f2066ee68d758c59
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-11-23 07:00:27 -0500

    Added ka to the ion ch distr., changed .csv filename

--- netParams_A.py
+++ netParams_A.py
@@ -12,16 +12,16 @@ genemod = {'ch_Cacna1a_cp5':{'gCav2_1bar': 0.00001},  'ch_Cacna1b_cp6':{'gCav2_2
            'ch_Cacna1c_cp3':{'gLbar': 0.0001},        'ch_Cacna1g_cp41':{'gCav3_1bar': 0.00001},
            'ch_Cacna1i_md279':{'gcabar': 0.00027},  'ch_Hcn1_cp9':{'gHCN1bar': 0.00001},         #pcabar =.2e-3; 'gCav3_3bar': 0.0001
            'ch_Hcn2_cp10':{'gHCN2bar': 0.009},           'ch_Hcn3_cp11':{'gHCN3bar': 0.0001},    
-           'ch_Hcn4_cp12':{'gHCN4bar': 0.0002},           'ch_Kcna1ab1_md80769':{'gbar': 0.015},   #0.015; 0.0054
+           'ch_Hcn4_cp12':{'gHCN4bar': 0.0002},           'ch_Kcna1ab1_md80769':{'gbar': 0.015},   #0.015; 0.0054 
+           'ka': {'gkabar': cfg.ka},
            'ch_Kcnc1_md74298':{'gk': 0.015},            'ch_Scn1a_md264834':{'gNav11bar': 1.0}}     #1.0    #0.015
-cell_identities = np.bool_(np.transpose(np.genfromtxt('allcells_new12_unique_binary.csv', delimiter=',')))
+cell_identities = np.bool_(np.transpose(np.genfromtxt('allcells_unique_binary.csv', delimiter=',')))
 cell = cell_identities[cfg.cellnum]
 
 ## Cell parameters/rules
 CEL = {'secs': {}}
 
-CEL['secs']['soma'] = {'geom': {'diam': 30, 'L': 30, 'Ra': 35.4, 'cm':1}, 'mechs':  {'pas' : {'g': 1.8e-6, 'e': -65}, 
-                                                                                    'ka': {'gkabar': cfg.ka}  } }
+CEL['secs']['soma'] = {'geom': {'diam': 30, 'L': 30, 'Ra': 35.4, 'cm':1}, 'mechs':  {'pas' : {'g': 1.8e-6, 'e': -65}} }
                                                                             
 #KAAR_rybak; gAbar
 
@@ -32,6 +32,6 @@ netParams.cellParams['CEL'] = CEL
 netParams.popParams['U'] = {'cellType': 'CEL', 'numCells': 1}
 
 if cfg.stim == 'IClamp':
-    netParams.stimSourceParams['iclamp'] = {'type': 'IClamp', 'amp': cfg.amp, 'dur': 400, 'delay': 100} 
+    netParams.stimSourceParams['iclamp'] = {'type': 'IClamp', 'amp': cfg.amp, 'dur': 500, 'delay': 100} 
     netParams.stimTargetParams['iclamp->CEL'] = {'source': 'iclamp', 'conds': {'cellType': 'CEL'}, 'sec': 'soma', 'loc': 0.5}
 
