d249ae94972d716cc41cd0328ea06ab023cd6f1a
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-11-23 06:59:25 -0500

    Changed labels, method, restored amp and cellnum

--- batch.py
+++ batch.py
@@ -8,16 +8,16 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        params['amp'] = [0.3] #, 0.6, 0.9] # indexed
-        params['cellnum'] = [0,1] #[x for x in range(101)] # indexed
+        params['amp'] = [0.6, 0.8, 1.0] # indexed
+        params['cellnum'] = [x for x in range(101)] # indexed
         #params['rybak'] = [0, 0.01, 0.03, 0.05, 0.07, 0.1, 0.12, 0.15] 
-        params['ka'] = [0] #[0.08, 0.1, 0.12, 0.15] 
+        params['ka'] = [0.08, 0.1, 0.12, 0.15] 
 
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_A.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21nov5n' 
+        b.batchLabel = '21nov23a' 
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
-        b.method = 'grid'  # params.csv will be read by default; else need name in cfg.paramListFile
+        b.method = 'list'  # params.csv will be read by default; else need name in cfg.paramListFile
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
         b.run()
 
