cb5faf6d07958c2a0ddf77e95a6fa8b01e5a4a44
Suranjana Gupta gupta8suranjana@gmail.com <suri@pi.neurosim.downstate.edu>
2021-11-30 03:54:20 -0500

    Running 2D sobol: Added cfg.na

--- netParams_A.py
+++ netParams_A.py
@@ -14,7 +14,7 @@ genemod = {'ch_Cacna1a_cp5':{'gCav2_1bar': 0.00001},  'ch_Cacna1b_cp6':{'gCav2_2
            'ch_Hcn2_cp10':{'gHCN2bar': 0.009},           'ch_Hcn3_cp11':{'gHCN3bar': 0.0001},    
            'ch_Hcn4_cp12':{'gHCN4bar': 0.0002},           'ch_Kcna1ab1_md80769':{'gbar': 0.015},   #0.015; 0.0054 
            'ka': {'gkabar': cfg.ka},
-           'ch_Kcnc1_md74298':{'gk': 0.015},            'ch_Scn1a_md264834':{'gNav11bar': 1.0}}     #1.0    #0.015
+           'ch_Kcnc1_md74298':{'gk': 0.015},            'ch_Scn1a_md264834':{'gNav11bar': cfg.na}}     #1.0    #0.015
 cell_identities = np.bool_(np.transpose(np.genfromtxt('allcells_unique_binary.csv', delimiter=',')))
 cell = cell_identities[cfg.cellnum]
 
