58d5ab61a34c1b19dd2765a40eeef2a7c34f84c7
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-12-13 18:38:21 -0500

    List pl stores indexed and log flags; print statement modified

--- sobol.py
+++ sobol.py
@@ -11,13 +11,13 @@ def parseBatchParams (b):
         print(f'Reading {os.getcwd()+"/"+b}')
     except Exception as e:
         print(f"ERROR >>>{e}<<<")
-    p = re.compile(r'''\s+params[^a-z]+([^]']+)'\]\s*=\s*(\[[^]]+\])\s*#*\s*(indexed)*''') # 'indexed' is the keyword to NOT sobolize eg amp or cellnum
+    p = re.compile(r'''\s+params[^a-z]+([^]']+)'\]\s*=\s*(\[[^]]+\])\s*#*\s*(indexed|log)*''') # 'indexed' is the keyword to NOT sobolize eg amp or cellnum
     bl, pl = [(i, p.match(l)) for i,l in enumerate(lines)], [] # bl: lines that match regexp
     for i,m in bl:
         if m:
             try:
                 pl.append((m.group(1), eval(m.group(2)), m.group(3))) # strings: name, valueList, [indexed]
-                print(f'\tsetting {m.group(1)} to {"indexed" if m.group(3) else "continuous"} in range {(lambda x:(min(x),max(x)))(eval(m.group(2)))}')
+                print(f'\tsetting {m.group(1)} to {m.group(3) if m.group(3) else "continuous"} in range {(lambda x:(min(x),max(x)))(eval(m.group(2)))}')
             except Exception as e:
                 print(f"ERROR >>>{e}<<<\n\tunable to evaluate '{m.group(2)}':\n\tline {i}: {m.string.strip()}")
     return pl
