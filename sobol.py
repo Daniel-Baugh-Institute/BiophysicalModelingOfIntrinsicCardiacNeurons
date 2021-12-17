983d70b0cc2e3b3e544febd3a11ba48310287ecc
Bill Lytton <wwlytton@yahoo.com>
2021-12-17 11:33:06 -0500

    can directly use infix operators with numpy arrays and can use eg 'np.log10()' for func call instead of regular log10

--- sobol.py
+++ sobol.py
@@ -33,10 +33,10 @@ def sobcall (pl, num, seed=33):
         elif v['type'] != 'indexed':
             raise Exception(f"{v['type']} unrecognized key word")
     sobpl = {k:v for k,v in pl.items() if v['type'] in ('linear',  'log')} # subset of pl without indexed tag
-    logcols = [i for i,v in enumerate(sobpl.values()) if v['type']=='log']
+    logcols = [i for i,v in enumerate(sobpl.values()) if v['type']=='log'] # col numbers for log
     sobolVals = sob(len(sobpl), num, seed=seed)
     scaledVals = qmc.scale(sobolVals, [v['min'] for v in sobpl.values()], [v['max'] for v in sobpl.values()]) # dict order guaranteed in py>=3.7; transpose
-    scaledVals[:,logcols] = list(map(lambda x:10**x, scaledVals[:,logcols]))
+    scaledVals[:,logcols] = 10**scaledVals[:,logcols]
     return (pl,sobpl,scaledVals,logcols)
     combos = [[*p[0],*p[1], *p[2]] for p in product(scaledVals.tolist(),scaledAlogVals.tolist(),list(product(*ivals)))]
     combos.insert(0, labels+llabels+ilabels)
