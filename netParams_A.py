939b1c560ec41764439b44c99530b04f43a13a49
Suranjana Gupta <gupta8suranjana@gmail.com>
2022-02-01 05:50:19 -0500

    Reading SP matrix; removed ka, add sze to vary size

--- netParams_A.py
+++ netParams_A.py
@@ -13,15 +13,14 @@ genemod = {'ch_Cacna1a_cp5':{'gCav2_1bar': cfg.c1a},  'ch_Cacna1b_cp6':{'gCav2_2
            'ch_Cacna1i_md279':{'gcabar': cfg.c1i},  'ch_Hcn1_cp9':{'gHCN1bar': cfg.h1},         #pcabar =.2e-3; 'gCav3_3bar': 0.0001
            'ch_Hcn2_cp10':{'gHCN2bar': cfg.h2},           'ch_Hcn3_cp11':{'gHCN3bar': cfg.h3},    
            'ch_Hcn4_cp12':{'gHCN4bar': cfg.h4},           'ch_Kcna1ab1_md80769':{'gbar': cfg.kcnab},   #0.015; 0.0054 
-           'ka': {'gkabar': cfg.ka},
            'ch_Kcnc1_md74298':{'gk': cfg.kcnc},            'ch_Scn1a_md264834':{'gNav11bar': cfg.na}}     #1.0    #0.015
-cell_identities = np.bool_(np.transpose(np.genfromtxt('allcells_unique_binary.csv', delimiter=',')))
+cell_identities = np.bool_(np.transpose(np.genfromtxt('sp_reduced.csv', delimiter=',')))
 cell = cell_identities[cfg.cellnum]
 
 ## Cell parameters/rules
 CEL = {'secs': {}}
 
-CEL['secs']['soma'] = {'geom': {'diam': 30, 'L': 30, 'Ra': 35.4, 'cm':1}, 'mechs':  {'pas' : {'g': 1.8e-6, 'e': -65}} }
+CEL['secs']['soma'] = {'geom': {'diam': cfg.sze, 'L': cfg.sze, 'Ra': 35.4, 'cm':1}, 'mechs':  {'pas' : {'g': 1.8e-6, 'e': -65}} }
                                                                             
 #KAAR_rybak; gAbar
 
@@ -35,3 +34,4 @@ if cfg.stim == 'IClamp':
     netParams.stimSourceParams['iclamp'] = {'type': 'IClamp', 'amp': cfg.amp, 'dur': 500, 'delay': 100} 
     netParams.stimTargetParams['iclamp->CEL'] = {'source': 'iclamp', 'conds': {'cellType': 'CEL'}, 'sec': 'soma', 'loc': 0.5}
 
+#           'ka': {'gkabar': cfg.ka},
