7eb1222bf1766156dc6e0309f7ef40a1c6bc97c8
Suranjana Gupta <gupta8suranjana@gmail.com>
2022-02-03 15:39:43 -0500

    Added Kcna model, cfg.sze, removed other kcnc, kcnab1, ca1i

--- netParams_A.py
+++ netParams_A.py
@@ -10,18 +10,16 @@ except:
 # order in genemod MUST be preserved to match cell_identities channel order
 genemod = {'ch_Cacna1a_cp5':{'gCav2_1bar': cfg.c1a},  'ch_Cacna1b_cp6':{'gCav2_2bar': cfg.c1b},
            'ch_Cacna1c_cp3':{'gLbar': cfg.c1c},        'ch_Cacna1g_cp41':{'gCav3_1bar': cfg.c1g},
-           'ch_Cacna1i_md279':{'gcabar': cfg.c1i},  'ch_Hcn1_cp9':{'gHCN1bar': cfg.h1},         #pcabar =.2e-3; 'gCav3_3bar': 0.0001
-           'ch_Hcn2_cp10':{'gHCN2bar': cfg.h2},           'ch_Hcn3_cp11':{'gHCN3bar': cfg.h3},    
-           'ch_Hcn4_cp12':{'gHCN4bar': cfg.h4},           'ch_Kcna1ab1_md80769':{'gbar': cfg.kcnab},   #0.015; 0.0054 
-           'ka': {'gkabar': cfg.ka},
-           'ch_Kcnc1_md74298':{'gk': cfg.kcnc},            'ch_Scn1a_md264834':{'gNav11bar': cfg.na}}     #1.0    #0.015
-cell_identities = np.bool_(np.transpose(np.genfromtxt('allcells_unique_binary.csv', delimiter=',')))
+           'ch_Hcn1_cp9':{'gHCN1bar': cfg.h1},         'ch_Hcn2_cp10':{'gHCN2bar': cfg.h2},           
+           'ch_Hcn3_cp11':{'gHCN3bar': cfg.h3},        'ch_Hcn4_cp12':{'gHCN4bar': cfg.h4},           
+           'ch_kcna_232813':{'gkcnabar': cfg.kcna},        'ch_Scn1a_md264834':{'gNav11bar': cfg.na}}     #1.0    #0.015
+cell_identities = np.bool_(np.transpose(np.genfromtxt('rat_allcells_binary.csv', delimiter=',')))
 cell = cell_identities[cfg.cellnum]
 
 ## Cell parameters/rules
 CEL = {'secs': {}}
 
-CEL['secs']['soma'] = {'geom': {'diam': 30, 'L': 30, 'Ra': 35.4, 'cm':1}, 'mechs':  {'pas' : {'g': 1.8e-6, 'e': -65}} }
+CEL['secs']['soma'] = {'geom': {'diam': cfg.sze, 'L': cfg.sze, 'Ra': 35.4, 'cm':1}, 'mechs':  {'pas' : {'g': 1.8e-6, 'e': -65}} }
                                                                             
 #KAAR_rybak; gAbar
 
