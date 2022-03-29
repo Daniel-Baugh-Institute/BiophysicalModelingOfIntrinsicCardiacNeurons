d97a331e9db12cc135fd8cef5bb0e88c278c495f
Suranjana Gupta <gupta8suranjana@gmail.com>
2022-03-29 05:09:27 -0400

    batch with gpas>4e-4 so that Rin~100MOhm

--- batch.py
+++ batch.py
@@ -10,7 +10,7 @@ def batch():
         params = specs.ODict()
         # params['amp'] = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5] # indexed
         params['cellnum'] = [x for x in range(104)] # indexed          all_15: 104
-        params['g']=[1e-4,2e-4,3e-4,4e-4]
+        params['g']=[4e-4,6e-4,7e-4,8e-4,9e-4]
         # params['phi']=[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
         # params['sze'] = [16,18,20,23,25,27,30] 
         # params['na'] = [0.1, 1]                 # log
@@ -19,7 +19,7 @@ def batch():
 
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_M1.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '22mar28c'
+        b.batchLabel = '22mar28d'
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'grid'  # params.csv will be read by default; else need name in cfg.paramListFile
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
