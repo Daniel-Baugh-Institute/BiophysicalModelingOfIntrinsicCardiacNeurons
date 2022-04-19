b69d9725a65621de8bbe9ab412bb7b8549ed3167
Suranjana Gupta <gupta8suranjana@gmail.com>
2022-04-19 12:35:13 -0400

    Changed gene seq, reading reordered csv, negative amp for Rin

--- netParams_M1.py
+++ netParams_M1.py
@@ -8,14 +8,15 @@ except:
     from cfg import cfg  # if no simConfig in parent module, import directly from cfg.py:cfg
 
 # order in genemod MUST be preserved to match cell_identities channel order
-genemod = {'ch_Cacna1a_cp5':{'gCav2_1bar': cfg.c1a},  'ch_Cacna1b_cp6':{'gCav2_2bar': cfg.c1b},
-           'ch_Cacna1c_cp3':{'gLbar': cfg.c1c},        'ch_Cacna1d_md150284':{'pcaLbar': cfg.c1d},
-           'ch_Cacna1g_cp41':{'gCav3_1bar': cfg.c1g},  'ch_Cacna1i_md279':{'gcabar': cfg.c1i},
-            'ch_Hcn1_cp9':{'gHCN1bar': cfg.h1},         'ch_Hcn2_cp10':{'gHCN2bar': cfg.h2},           
-            'ch_Hcn3_cp11':{'gHCN3bar': cfg.h3},        'ch_Hcn4_cp12':{'gHCN4bar': cfg.h4},           
-            'ch_Kcna1ab1_md80769':{'gbar': cfg.ka},  'ch_Kcnc1_rothman':{'gbar': cfg.kc, 'phi':cfg.phi},  #0.0011          
-            'ch_Kcnj3_md2488':{'gbar': cfg.kj},        'ch_Scn1a_md264834':{'gNav11bar': cfg.na}}     #0.0015
-cell_identities = np.bool_(np.transpose(np.genfromtxt('red_tdata_all_15.csv', delimiter=',')))
+genemod = {'ch_Hcn3_cp11':{'gHCN3bar': cfg.h3},  'ch_Hcn1_cp9':{'gHCN1bar': cfg.h1},
+           'ch_Cacna1g_cp41':{'gCav3_1bar': cfg.c1g},        'ch_Cacna1a_cp5':{'gCav2_1bar': cfg.c1a},
+           'ch_Cacna1d_md150284':{'pcaLbar': cfg.c1d},  'ch_Hcn4_cp12':{'gHCN4bar': cfg.h4},
+            'ch_Cacna1c_cp3':{'gLbar': cfg.c1c},         'ch_Cacna1i_md279':{'gcabar': cfg.c1i},           
+            'ch_Kcna1ab1_md80769':{'gbar': cfg.ka},        'ch_Kcnj3_md2488':{'gbar': cfg.kj},           
+            'ch_Cacna1b_cp6':{'gCav2_2bar': cfg.c1b},  'ch_Hcn2_cp10':{'gHCN2bar': cfg.h2},  #0.0011          
+            'ch_Kcnc1_rothman':{'gbar': cfg.kc, 'phi':cfg.phi}, 'ch_Scn1a_md264834':{'gNav11bar': cfg.na}}     #0.0015
+
+cell_identities = np.bool_(np.transpose(np.genfromtxt('cell_sort_m2l.csv', delimiter=',')))
 cell = cell_identities[cfg.cellnum]
 
 ## Cell parameters/rules
