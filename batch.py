541a1924eb2761834a440106b559f99c97eb5f39
Suranjana Gupta <gupta8suranjana@gmail.com>
2022-05-25 13:22:57 -0400

    Amp: Vary amps

--- batch.py
+++ batch.py
@@ -8,7 +8,7 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        params['amp'] = [-1e-7] #[0.01, 0.05, 0.1, 0.3, 0.6, 0.8] # indexed
+        params['amp'] = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1] #indexed
         params['cellnum'] = [x for x in range(104)] # indexed          all_15: 104
         # params['phi']=[0,0.2,1] # indexed
         # params['na'] = [0.8, 1]
@@ -30,7 +30,7 @@ def batch():
 
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_M1.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '22may25a'
+        b.batchLabel = '22may25b'
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'grid'  # params.csv will be read by default; else need name in cfg.paramListFile
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
