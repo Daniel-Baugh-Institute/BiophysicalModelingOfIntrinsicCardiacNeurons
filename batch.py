863e0428c8bad3006789220c361a9ef767b44dcc
Suranjana Gupta gupta8suranjana@gmail.com <suri@is.neurosim.downstate.edu>
2022-05-26 11:59:29 -0400

    Time taken for a single batch run/job

--- batch.py
+++ batch.py
@@ -8,10 +8,10 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        # params['amp'] = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1] #indexed
-        params['cellnum'] = [x for x in range(104)] # indexed          all_15: 104
+        params['amp'] = [0.05] #[0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1] #indexed
+        params['cellnum'] = [0] #[x for x in range(104)] # indexed          all_15: 104
         # params['phi']= [0,0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] # indexed
-        params['na'] = [0.1, 1]  
+        # params['na'] = [0.1, 1]  
         # params['ka'] = [0.0018, 0.058] #[0.011, 0.014, 0.018, 0.025, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.06]
         # params['kc'] = [0.00018, 0.0018, 0.018, 0.18] #[0.00018, 0.18]        #log
         # params['kj'] = [0.00018, 0.0038]
@@ -19,7 +19,7 @@ def batch():
         # params['h2'] = [0.009, 0.09]           
         # params['h3'] = [0.0001, 0.01]           #log
         # params['h4'] = [0.0002, 0.02]           #log
-        params['c1i'] = [0.0001, 0.0003, 0.0005, 0.0007, 0.0009, 0.001] #[0.00027, 0.001]
+        # params['c1i'] = [0.0001, 0.0003, 0.0005, 0.0007, 0.0009, 0.001] #[0.00027, 0.001]
         # params['c1g'] = [0.00001, 0.001]        #log
         # params['c1d'] = [0.7e-4, 9.7e-4]
         # params['c1c'] = [0.0001, 0.01]           #log
@@ -30,7 +30,7 @@ def batch():
 
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_M1.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '22may25g'
+        b.batchLabel = '22may26a'
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'grid'  # params.csv will be read by default; else need name in cfg.paramListFile
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
