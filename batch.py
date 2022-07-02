1d854e777a053fdafa8a5a17a738fd869dd9a983
Suranjana Gupta gupta8suranjana@gmail.com <suri@is.neurosim.downstate.edu>
2022-07-02 12:01:15 -0400

    Reduced amp and phi

--- batch.py
+++ batch.py
@@ -8,9 +8,9 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        params['amp'] = [0.01, 0.02,0.03, 0.04,0.05, 0.06,0.07, 0.08,0.09] # indexed
+        params['amp'] = [0.01,0.03,0.05,0.07,0.09] # indexed
         params['cellnum'] = [x for x in range(104)] # indexed          all_15: 104
-        params['phi']= [0,0.1,0.2,0.3,0.4, 0.5, 0.6,0.7,0.8,0.9,1] # indexed
+        params['phi']= [0.2,0.5,0.85] # indexed
         params['na'] = [0.09,0.18]  
         params['ka'] = [0.015, 0.023]                  #[0.011, 0.014, 0.018, 0.025, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.06]
         params['kc'] = [0.011, 0.02]          #[0.00018, 0.00048, 0.00078, 0.0018, 0.0048, 0.0078, 0.018, 0.048, 0.078, 0.18]             
