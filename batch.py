ea0ac7df51a859e2507f8779674982567dd64a28
Suranjana Gupta <gupta8suranjana@gmail.com>
2022-08-24 10:25:56 -0400

    h1 inspect transtion from active to passive

--- batch.py
+++ batch.py
@@ -9,13 +9,13 @@ from netpyne.batch import Batch
 def batch():
         params = specs.ODict()
         params['amp'] = [0.1] # indexed 
-        params['cellnum'] = [0,11,52,101] #[x for x in range(104)] # indexed          all_15: 104
+        params['cellnum'] = [0] #[x for x in range(104)] # indexed          all_15: 104
         # params['phi']= [0.2] 
         params['na'] = [0.075]  
         params['ka'] = [0.018] 
         params['kc'] = [0.018]                               
         params['kj'] = [0.0035]
-        params['h1'] = [0.003]           
+        params['h1'] = [0.01, 0.03, 0.05, 0.07, 0.1]           
         params['h2'] = [0.009]           
         params['h3'] = [0.01]           
         params['h4'] = [0.0035]           
@@ -24,14 +24,14 @@ def batch():
         params['c1d'] = [0.00045]
         params['c1c'] = [0.006]           
         params['c1b'] = [0.0001]          
-        params['c1a'] = [0.00001, 0.0001, 0.001, 0.01, 0.1]         
+        params['c1a'] = [0.00033]         
         # params['g']=[5e-4,6e-4,7e-4,8e-4,9e-4]        # indexed
         # params['vc']= [v for v in np.arange(-100,101,10)]             # indexed
 
 
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_M1.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '22aug23n'
+        b.batchLabel = '22aug23o'
         b.saveFolder = b.batchLabel #'/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'grid'  # params.csv will be read by default; else need name in cfg.paramListFile
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
