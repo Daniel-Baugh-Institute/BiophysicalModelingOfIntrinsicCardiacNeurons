69c87098c0b677b82499dce94fd06cfbd4495286
Suranjana Gupta <gupta8suranjana@gmail.com>
2022-08-25 16:58:58 -0400

    Model P: C1a cond fixed, Vclamp sims for IV

--- batch.py
+++ batch.py
@@ -8,7 +8,7 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        params['amp'] = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5] # indexed 
+        # params['amp'] = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5] # indexed 
         params['cellnum'] = [x for x in range(104)] # indexed          all_15: 104
         # params['phi']= [0,0.2,0.6,1] # indexed  
         # params['na'] = [0.076, 0.114]  
@@ -26,12 +26,12 @@ def batch():
         # params['c1b'] = [0.0004, 0.0006]                  
         # params['c1a'] = [0.00008, 0.00012]        
         # params['g']=[5e-4,6e-4,7e-4,8e-4,9e-4]        # indexed
-        # params['vc']= [v for v in np.arange(-100,101,10)]             # indexed
+        params['vc']= [v for v in np.arange(-100,101,10)]             # indexed
 
 
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_P.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '22aug25b'
+        b.batchLabel = '22aug25d'
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'grid'  # params.csv will be read by default; else need name in cfg.paramListFile
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
