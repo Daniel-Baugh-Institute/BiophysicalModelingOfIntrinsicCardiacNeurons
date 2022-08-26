ecccb75a45f3a9bc39f53a34bbe7995acafbe7e8
Suranjana Gupta <gupta8suranjana@gmail.com>
2022-08-26 07:26:56 -0400

    Model P: C1a cond fixed, amp+phi based

--- batch.py
+++ batch.py
@@ -8,9 +8,9 @@ from netpyne.batch import Batch
 
 def batch():
         params = specs.ODict()
-        # params['amp'] = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5] # indexed 
+        params['amp'] = [0.08, 0.1, 0.2, 0.3] # indexed 
         params['cellnum'] = [x for x in range(104)] # indexed          all_15: 104
-        # params['phi']= [0,0.2,0.6,1] # indexed  
+        params['phi']= ['phi']= [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1] #[0,0.2,0.6,1] # indexed  
         # params['na'] = [0.076, 0.114]  
         # params['ka'] = [0.0144, 0.0216] 
         # params['kc'] = [0.08, 0.12]                               
@@ -26,12 +26,12 @@ def batch():
         # params['c1b'] = [0.0004, 0.0006]                  
         # params['c1a'] = [0.00008, 0.00012]        
         # params['g']=[5e-4,6e-4,7e-4,8e-4,9e-4]        # indexed
-        params['vc']= [v for v in np.arange(-100,101,10)]             # indexed
+        # params['vc']= [v for v in np.arange(-100,101,10)]             # indexed
 
 
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_P.py')
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '22aug25d'
+        b.batchLabel = '22aug25c'
         b.saveFolder = '/tera/' + os.getlogin() + '/' + b.batchLabel
         b.method = 'grid'  # params.csv will be read by default; else need name in cfg.paramListFile
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
--- cfg.py
+++ cfg.py
@@ -8,35 +8,35 @@ cfg.duration = 1000  #1500 #1*1e3 #(ms)
 cfg.dt = 0.025          
 cfg.verbose = False     
 cfg.recordCells = ['all']
-cfg.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'},
+cfg.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'}}
                     # 'm_Na':{'sec': 'soma','loc': 0.5,'var': 'm_ch_Scn1a_cp35'},
                     # 'h_Na':{'sec': 'soma','loc': 0.5,'var': 'h_ch_Scn1a_cp35'}}
                          #'cai':{'sec': 'soma','loc': 0.5,'var': 'cai'}}
                        # 'epas' : {'sec': 'soma', 'loc': 0.5,'var': 'e_pas'},
-                    'ih':{'sec': 'soma', 'loc': 0.5,'var': 'iother'}, 
-                    'ina': {'sec': 'soma', 'loc': 0.5,'var': 'ina'},
-                    'ica': {'sec': 'soma', 'loc': 0.5,'var': 'ica'},
-                    'ik': {'sec': 'soma', 'loc': 0.5,'var': 'ik'},
-                    'ipas':{'sec': 'soma', 'loc': 0.5,'var': 'i_pas'},
-                    'ica1a':{'sec': 'soma', 'loc': 0.5,'var': 'ica1a_ch_Cacna1a_cp5'},
-                    'ica1b':{'sec': 'soma', 'loc': 0.5,'var': 'ica1b_ch_Cacna1b_cp6'},
-                    'ica1c':{'sec': 'soma', 'loc': 0.5,'var': 'ica1c_ch_Cacna1c_cp3'},
-                    'ica1d':{'sec': 'soma', 'loc': 0.5,'var': 'ica1d_ch_Cacna1d_md150284'},
-                    'ica1g':{'sec': 'soma', 'loc': 0.5,'var': 'ica1g_ch_Cacna1g_cp41'},
-                    'ica1i':{'sec': 'soma', 'loc': 0.5,'var': 'ica1i_ch_Cacna1i_md279'},
-                    'ihcn1':{'sec': 'soma', 'loc': 0.5,'var': 'ihcn1_ch_Hcn1_cp9'},
-                    'ihcn2':{'sec': 'soma', 'loc': 0.5,'var': 'ihcn2_ch_Hcn2_cp10'},
-                    'ihcn3':{'sec': 'soma', 'loc': 0.5,'var': 'ihcn3_ch_Hcn3_cp11'},
-                    'ihcn4':{'sec': 'soma', 'loc': 0.5,'var': 'ihcn4_ch_Hcn4_cp12'},
-                    'ikcna':{'sec': 'soma', 'loc': 0.5,'var': 'ikcna_ch_Kcna1ab1_md80769'},
-                    'ikcnc':{'sec': 'soma', 'loc': 0.5,'var': 'ikcnc_ch_Kcnc1_rothman'},
-                    'ikcnj3':{'sec': 'soma', 'loc': 0.5,'var': 'ikcnj3_ch_Kcnj3_md2488'}}
+                    # 'ih':{'sec': 'soma', 'loc': 0.5,'var': 'iother'}, 
+                    # 'ina': {'sec': 'soma', 'loc': 0.5,'var': 'ina'},
+                    # 'ica': {'sec': 'soma', 'loc': 0.5,'var': 'ica'},
+                    # 'ik': {'sec': 'soma', 'loc': 0.5,'var': 'ik'},
+                    # 'ipas':{'sec': 'soma', 'loc': 0.5,'var': 'i_pas'},
+                    # 'ica1a':{'sec': 'soma', 'loc': 0.5,'var': 'ica1a_ch_Cacna1a_cp5'},
+                    # 'ica1b':{'sec': 'soma', 'loc': 0.5,'var': 'ica1b_ch_Cacna1b_cp6'},
+                    # 'ica1c':{'sec': 'soma', 'loc': 0.5,'var': 'ica1c_ch_Cacna1c_cp3'},
+                    # 'ica1d':{'sec': 'soma', 'loc': 0.5,'var': 'ica1d_ch_Cacna1d_md150284'},
+                    # 'ica1g':{'sec': 'soma', 'loc': 0.5,'var': 'ica1g_ch_Cacna1g_cp41'},
+                    # 'ica1i':{'sec': 'soma', 'loc': 0.5,'var': 'ica1i_ch_Cacna1i_md279'},
+                    # 'ihcn1':{'sec': 'soma', 'loc': 0.5,'var': 'ihcn1_ch_Hcn1_cp9'},
+                    # 'ihcn2':{'sec': 'soma', 'loc': 0.5,'var': 'ihcn2_ch_Hcn2_cp10'},
+                    # 'ihcn3':{'sec': 'soma', 'loc': 0.5,'var': 'ihcn3_ch_Hcn3_cp11'},
+                    # 'ihcn4':{'sec': 'soma', 'loc': 0.5,'var': 'ihcn4_ch_Hcn4_cp12'},
+                    # 'ikcna':{'sec': 'soma', 'loc': 0.5,'var': 'ikcna_ch_Kcna1ab1_md80769'},
+                    # 'ikcnc':{'sec': 'soma', 'loc': 0.5,'var': 'ikcnc_ch_Kcnc1_rothman'},
+                    # 'ikcnj3':{'sec': 'soma', 'loc': 0.5,'var': 'ikcnj3_ch_Kcnj3_md2488'}}
 
-cfg.stim = 'VClamp' #'IClamp'  
+cfg.stim = 'IClamp' #'IClamp'  
 cfg.cellnum = 52
 cfg.sze = 21
-# cfg.amp = 0.5 #-1e-5 #0.6
-cfg.vc = 0 
+cfg.amp = 0.5 #-1e-5 #0.6
+# cfg.vc = 0 
 
 # cfg.na = 0.075 #1
 
