5729f25e26c15653ebc8ac170031ed4676b580d2
adam newton <adam.newton@yale.edu>
2022-09-19 14:49:19 -0400

    Fix initialization.

--- init.py
+++ init.py
@@ -2,32 +2,33 @@ from netpyne import sim
 from neuron import h
 
 
-def fi(seg):
+def fi(cells):
     """set steady state RMP for 1 cell"""
-    print(seg.v)
-    isum = 0
-    isum = (
-        (seg.ina if h.ismembrane("na_ion") else 0)
-        + (seg.ik if h.ismembrane("k_ion") else 0)
-        + (seg.ica if h.ismembrane("ca_ion") else 0)
-        + (seg.iother if h.ismembrane("other_ion") else 0)
-    )
-    seg.e_pas = cfg.hParams["v_init"] + isum / seg.g_pas
-    if h.ismembrane("cadad"):
-        seg.cainf_cadad = seg.cai - (
-            (-(10000) * seg.ica / (2 * h.FARADAY * seg.depth_cadad)) * seg.taur_cadad
+    for c in cells:
+        seg = c.secs.soma.hObj(0.5)
+        isum = 0
+        isum = (
+            (seg.ina if h.ismembrane("na_ion") else 0)
+            + (seg.ik if h.ismembrane("k_ion") else 0)
+            + (seg.ica if h.ismembrane("ca_ion") else 0)
+            + (seg.iother if h.ismembrane("other_ion") else 0)
         )
-    for modulation in netParams.neuromod.values():
-        for mod, param in modulation.items():
-            if hasattr(seg, mod):
-                for k, v in param.items():
-                    setattr(getattr(seg, mod), k, v)
+        seg.e_pas = cfg.hParams["v_init"] + isum / seg.g_pas
+        if h.ismembrane("cadad"):
+            seg.cainf_cadad = seg.cai - (
+                (-(10000) * seg.ica / (2 * h.FARADAY * seg.depth_cadad)) * seg.taur_cadad
+            )
+        for modulation in netParams.neuromod.values():
+            for mod, param in modulation.items():
+                if hasattr(seg, mod):
+                    for k, v in param.items():
+                        setattr(getattr(seg, mod), k, v)
 
 
 def simSim(np0, sc0):
 
     sim.create(netParams=np0, simConfig=sc0)
-    fih = [h.FInitializeHandler(2, lambda: fi(sim.net.cells[0].secs.soma.hObj(0.5)))]
+    fih = [h.FInitializeHandler(2, lambda: fi(sim.net.cells))]
     print("BEFORE simulate")
     sim.simulate()
     sim.analyze()
