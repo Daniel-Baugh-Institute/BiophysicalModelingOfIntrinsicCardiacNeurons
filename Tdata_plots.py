57926f1e3e55a7adea3a71aa0bc5e106cc4c7e5b
Michelle Gee <mmgee@udel.edu>
2023-03-23 11:46:04 -0400

    Highlight commonly occuring neuronal-types

--- Tdata_plots.py
+++ Tdata_plots.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 17:17:01 2023
+
+@author: mmgee
+"""
+
 import numpy as np
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 import plotly.graph_objects as go
-import plotly_express as px
+# import plotly_express as px
 import math as m
 import plotly.io as pio
 import seaborn as sns
@@ -82,7 +89,7 @@ ty = 14.8
 txt1 = 'Neuronal-Type ID: T1'
 txt2 = 'T104'
 cs = 'binary'
-phys =["HCN3","HCN1","Cav 3.1 (T)","Cav 2.1 (P/Q)","Cav 1.3 (L)","HCN4","Cav 1.2 (L)","Cav 3.3 (T)","Kv 1.1","Kir 3.1","Cav 2.2 (N)","HCN2","Kv 3.1","Nav 1.1"]
+phys =["HCN3 (h)","HCN1 (h)","Cav 3.1 (T)","Cav 2.1 (P/Q)","Cav 1.3 (L)","HCN4 (h)","Cav 1.2 (L)","Cav 3.3 (T)","Kv 1.1 (Kdr)","Kir 3.1 (Kdr)","Cav 2.2 (N)","HCN2 (h)","Kv 3.1 (Kdr)","Nav 1.1 (Naf)"]
 
 plotHeatmap(dfrm2l,gr,phys,'red',wr,tx1, txe, ty,txt1,txt2,cs)
 
@@ -136,24 +143,49 @@ dovr['Overall'] = freqBar(df104.transpose(),draw.transpose(),'')
 print(f"No. of cells wrt frequency \n {dovr.value_counts()}")
 print(f"Summary statistics \n {dovr.describe()}")
 
+# recolor T4, T7, T14, T15, T30, T73, T91 in heatmap by replacing 1 in gene expression with -1
+df_new = dfrm2l
+common_type = [4, 7, 14, 15, 23, 30, 73, 91] # indices of commonly occuring neuronal-types
+for i in common_type:
+    df_new.iloc[[i-1],:]=-1 * df_new.iloc[[i-1],:]
+
 fnt = 25
 plt.rcParams['font.size']=fnt
 f,axs = plt.subplots(2,1, figsize=(25,15))
 sns.set(font_scale = 1.8)
-sns.heatmap(dfrm2l.transpose(),linewidth = 0.4,xticklabels=False,yticklabels = gr,cmap=cs,cbar=False, ax=axs[0])
+sns.heatmap(dfrm2l.transpose(),linewidth = 0.4,xticklabels=False,yticklabels = gr,cmap="PiYG",cbar=False, ax=axs[0])
 axs[0].set_ylabel('Genomic Identity',fontsize=fnt)
 axs[0].set_yticklabels(axs[0].get_ymajorticklabels(), fontsize = fnt)
 ax2 = axs[0].twinx()
 ax2.yaxis.set_ticks_position('right')
-ax2 = sns.heatmap(dfrm2l.transpose(),linewidth = 0.5,xticklabels=False,yticklabels = phys,cmap=cs,cbar=False)
+ax2 = sns.heatmap(dfrm2l.transpose(),linewidth = 0.5,xticklabels=False,yticklabels = phys,cmap="PiYG",cbar=False)
 ax2.set_ylabel('Physiological Identity',fontsize=fnt)
 ax2.set_yticklabels(ax2.get_ymajorticklabels(), fontsize = fnt)
 plt.text(tx1,ty,txt1, fontdict={'size':fnt})
 plt.text(txe,ty,txt2, fontdict={'size':fnt})
-sns.barplot(x = dovr.index+1, y = dovr['Overall'],color='mediumseagreen', ax=axs[1])
+
+# bar plot
+cols = ['mediumvioletred' if z > 10 else 'green' for z in dovr['Overall']]
+sns.barplot(x = dovr.index+1, y = dovr['Overall'],palette=cols, ax=axs[1])
 axs[1].set_xticks(np.arange(1, 104, step=5))
 axs[1].set_xlabel('Neuronal-Type ID (T#)',fontsize=fnt)
 axs[1].set_ylabel('Number of Occurences',fontsize=fnt)
 plt.tight_layout()
 plt.savefig("tdata_red_freq.png",dpi=300,bbox_inches='tight')
 # plt.show()
+
+# determine number of neuronal types and number of total cells (occurences) that lack T channel (Cacna1g) and Hcn1
+# Cacna1g = df_new.iloc[:,2]
+# Cacna1g_idx = df_new.iloc[:,2]#.gt(0.5) # vector of true/false for indices where Cacna1g is present
+df_array = df_new.to_numpy()
+df_array = np.absolute(df_array) # change -1's inserted for plotting to 1's
+df_num = pd.DataFrame(df_array)
+Cacna1g_idx = df_num.index[df_num.iloc[:,2]>0].tolist() # Cacna1g index = 2
+Hcn1_idx = df_num.index[df_num.iloc[:,1]>0].tolist() # Hcn1 index = 1
+cell_occur_vec_Cacna1g = dovr.loc[Cacna1g_idx,:]
+with_Cacna1g = cell_occur_vec_Cacna1g.sum(axis=0)
+without_Cacna1g = 321-with_Cacna1g
+
+cell_occur_vec_Hcn1 = dovr.loc[Hcn1_idx,:]
+with_Hcn1 = cell_occur_vec_Hcn1.sum(axis=0)
+without_Hcn1 = 321-with_Hcn1
