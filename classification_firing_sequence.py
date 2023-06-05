8c53c9e75a9f35f287290e5b3b422a2db02366b3
michmgee <78487248+michmgee@users.noreply.github.com>
2023-06-05 11:46:19 -0400

    Add files via upload
    Modified classification function in scAnalysis to sort by firing sequence

---
+++ classification_firing_sequence.py
@@ -0,0 +1,284 @@
+import pandas as pd
+import numpy as np
+import plotly_express as px
+from os import makedirs
+import re
+from collections import OrderedDict
+from itertools import product
+import plotly.graph_objects as go
+import plotly_express as px
+import plotly.io as pio
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+from matplotlib_scalebar.scalebar import ScaleBar
+
+# Function to plot time vs voltage curves
+def plotVm(df,batchLabel):
+    #makedirs('Jun0123PB/vmPlots')
+    font = 15
+    f, ax = plt.subplots()
+    plt.axis("off")
+    offset = 0
+    for indx in df.index:
+        y = df['Vlist'][indx]
+        y = np.array(y)
+        y = 3*(offset)+y # add offset for ease of viewing
+        x = df['t'][indx]
+        x = np.array(x)
+        x = 25*(offset) + x #add offset
+        plt.plot(x,y,linewidth=1.5)
+        offset = offset + 1
+    plt.hlines(-20.0,900,1000,label='100 ms',colors=['black'])
+    plt.vlines(900.0,-20,-10,label="10 mV",colors=['black'])
+    plt.text(865,-25,'100 ms',fontsize=font)
+    plt.text(725,-18,'10 mV',fontsize=font)
+    plt.title(f"Tonic, Neuronal-Type ID: T{df['cellnum'][indx]+1}", fontsize=font)
+    plt.tick_params(axis='both',labelsize=font)
+    plt.legend(['0.1','0.3','0.5'])
+    plt.savefig(f"Jun0123P/vmPlots/{batchLabel}.png",dpi=300,bbox_inches='tight')
+    plt.close()
+    return
+
+# Produces bar chart of firing sequences vs counts
+def classify_sequence(dc):
+    dc['pbt'] = dc.apply(lambda x: 
+                            0 if x['Phasic'] == 1 
+                            else (1 if x['Burst'] == 1 
+                                else (2 if x['Tonic'] == 1 
+                                        else (3 if x['Subthreshold'] == 1 
+                                            else 4))), 
+                        axis=1)
+
+    dc["pbt_list"] = "" # create a new column "pbt_seq" and initialize it with empty strings
+    temp_list = []
+
+    dc = dc.reset_index(drop=True)
+    for index, row in dc.iterrows():
+        if index == 0:
+            temp_list.append(dc.loc[index,"pbt"])
+            dc.loc[index, 'pbt_list'] = temp_list
+        elif (row["cellnum"] == dc.iloc[index-1]["cellnum"]) and (row["na"] == dc.iloc[index-1]["na"]):
+            temp_list.append(dc.loc[index, "pbt"])
+            dc.at[index, 'pbt_list'] = temp_list
+        else:
+            temp_list = []
+            temp_list.append(dc.loc[index, "pbt"])
+            dc.at[index, 'pbt_list'] = temp_list
+            
+    dc.at[len(dc)-1, "pbt_list"] = temp_list # last value in the temp_list
+    a = dc['pbt_list']
+
+
+
+    num_rows = dc.shape[0]/5
+    num_rows = int(num_rows)
+    shape = [num_rows, 1]  # shape of the array
+    seq_cat = np.empty(shape)
+    df_temp = pd.DataFrame(columns=['rseq'])
+    index_t = 0
+    index_p = 0
+    index_pt = 0
+    index_tp = 0
+    index_tb = 0
+    tb_idx = []
+    ptb_idx = []
+    pbtb_idx = []
+    pbp_idx = []
+    pb_idx = []
+    tbp_idx = []
+    ptp_idx = []
+    bp_idx = []
+    ptbt_idx = []
+
+    for index in range(num_rows):
+        # loop through sections of 5 (for each of the 5 input stimulus) and make new array with sequence of firing patterns
+        seq = np.array(dc.iloc[5*index:5*index+5]['pbt'])
+
+        # Remove consecutive duplicates and assign to new array, rseq
+        rseq = []
+        for i in seq:
+            if len(rseq) == 0 or i != rseq[-1]:
+                rseq.append(i)
+        
+        rseq = np.array([rseq])
+        df_temp.at[index,'rseq'] = rseq
+
+        # Classify rseq into categories: 'Phasic', 'PB', 'PBP', 'PBT','PBTP', 'PBTB','Phasic-Tonic','PTB','PTP','PTBT','BP','Tonic','Tonic-Phasic','TB','TBP','SP','Other'
+        if rseq.size == 0:
+            print('Uh oh, rseq is empty!')
+        elif 4 in rseq:
+            seq_cat[index] = 16
+            print('Post-stimulus firing, tonic block, depolarisation block, or incomplete repolarisation occured')
+        elif rseq.size == 1:
+            if rseq[0][0] == 0: #p
+                seq_cat[index] = 0
+                if index_p == 0:
+                    print('P')
+                    print(index*5)
+                    index_p = index_p + 1
+            elif rseq[0][0] == 2: #t
+                seq_cat[index] = 11
+                if index_t == 0:
+                    print('T')
+                    print(index*5)
+                    index_t = index_t + 1
+            else:
+                print('No category at')
+                print(index)
+        elif rseq[0][0] == 1: #bp
+            seq_cat[index] = 10
+            bp_idx.append(index*5)
+        elif rseq[0][0] == 3: #sp
+            seq_cat[index] = 15
+        elif rseq[0][0] == 2: # starts with tonic, but not tonic only
+            if rseq[0][1] == 0 and rseq.size == 2: # tp
+                seq_cat[index] = 12
+                if index_tp == 0:
+                    print('TP')
+                    print(index*5)
+                    index_tp = index_tp + 1
+            elif rseq.size == 2: # tb
+                seq_cat[index] = 13
+                tb_idx.append(index*5)
+                index_tb = index_tb + 1
+            elif rseq.size == 3: # tbp
+                seq_cat[index] = 14
+                tbp_idx.append(index*5)
+            else:
+                print('No category at')
+                print(index)
+        else: # starts with phasic, but not phasic only
+            if rseq.size == 2:
+                if rseq[0][1] == 1: # pb
+                    seq_cat[index] = 1
+                    pb_idx.append(index*5)
+                elif rseq[0][1] == 2: # pt
+                    seq_cat[index] = 6
+                    if index_pt == 0:
+                        print(index*5)
+                        index_pt = index_pt + 1
+                else:
+                    print('No category at')
+                    print(index)
+            elif rseq.size == 3:
+                if rseq[0][1] == 1:
+                    if rseq[0][2] == 0: # pbp
+                        seq_cat[index] = 2
+                        pbp_idx.append(index*5)
+                    elif rseq[0][2] == 2: # pbt
+                        seq_cat[index] = 7
+                    else:
+                        print('No category at')
+                        print(index)
+                else:
+                    if rseq[0][2] == 0: #ptp
+                        seq_cat[index] = 8
+                    elif rseq[0][2] == 1: # ptb
+                        seq_cat[index] = 7
+                        #ptb_idx.append(index*5)
+                    else:
+                        print('No category at')
+                        print(index)
+            else:
+                if rseq[0][2] == 1: #ptbt
+                    seq_cat[index] = 9
+                    ptbt_idx.append(index*5)
+                elif rseq[0][3] == 1: # pbtb
+                    seq_cat[index] = 5
+                    pbtb_idx.append(index*5)
+                elif rseq[0][3] == 2: #pbtp
+                    seq_cat[index] = 4   
+                else: 
+                    print('No category at')
+                    print(index) 
+    
+    # attempted to programmatically determine sequence of firing patterns, but the code doesn't seem to work
+    unique_series = df_temp.duplicated(subset='rseq', keep='first')
+    filtered_df = df_temp[~unique_series]
+    unique_series = filtered_df.duplicated(subset='rseq', keep='first')
+    filtered_df = filtered_df[~unique_series]
+
+    # make new dataframe with the count of each firing pattern
+    df_seq = pd.DataFrame(columns = ['Firing pattern','Count'])
+    df_seq['Firing pattern'] = ['Phasic', 'PB', 'PBP', 'PBT','PBTP', 'PBTB','Phasic-Tonic','PTB','PTP','PTBT','BP','Tonic','Tonic-Phasic','TB','TBP','SP','Other']
+
+    # count number of each firing sequence
+    firing_behavior = range(0,17)
+    count_col = []
+    for index in range(len(firing_behavior)):
+        count = np.count_nonzero(seq_cat == index)
+        count_col.append(count)
+
+    print(count_col)
+    df_seq['Count'] = count_col
+    df_seq = df_seq[df_seq['Count'] > 0] # Change this threshold to plot only commonly occuring firing sequences
+    df_seq = df_seq.sort_values(by = 'Count',ascending=False)
+    print("Dataframe size:", df_seq.shape)
+    df_seq.to_json('df_seq.json')
+
+    # Plot bar chart
+    fig = px.bar(df_seq,x='Firing pattern',y='Count')
+    fig.update_layout(
+        font=dict(
+            size=18
+        )
+    )
+    fig.write_image("firing_bar.png")
+    fig.show()
+    return
+
+# load data
+dc = pd.read_json("C:\\Users\\mmgee\\Downloads\\classification_test.json")
+
+# Run sequence classification
+classify_sequence(dc)
+
+# Plot and example voltage vs time for each common type
+# Phasic only
+# start = 0
+# idx = np.linspace(start,start+4,3)
+# df = dc.loc[idx,['Vlist','t','cellnum']]
+# batchLabel = "Phasic" #
+# plotVm(df,batchLabel)
+
+# PT
+# start = 10
+# idx = np.linspace(start,start+4,3)
+# df = dc.loc[idx,['Vlist','t','cellnum']]
+# batchLabel = "Phasic-Tonic" #
+# plotVm(df,batchLabel)
+
+# # TP
+# start = 1990
+# idx = np.linspace(start,start+4,3)
+# df = dc.loc[idx,['Vlist','t','cellnum']]
+# batchLabel = "Tonic-Phasic" #
+# plotVm(df,batchLabel)
+
+# T
+start = 2000
+idx = np.linspace(start,start+4,3)
+df = dc.loc[idx,['Vlist','t','cellnum']]
+batchLabel = "Tonic"
+plotVm(df,batchLabel)
+
+# Use this to generate multiple plots at once
+# start = 3780
+# idx = np.linspace(start,start+4,3)
+# df = dc.loc[idx,['Vlist','t','cellnum']]
+# makedirs('Jun0123PTBT/vmPlots')
+# for i in range(len(ptbt_idx)-1):
+#     df = dc.loc[ptbt_idx[i]:ptbt_idx[i]+4,['Vlist','t','cellnum']]
+#     batchLabel = f"Jun0123PTBT_{ptbt_idx[0]}"
+#     plotVm(df,batchLabel)
+
+"""
+Notes on what is categorized as burst but is actually depolarisation block
+# PTB is really phasic to tonic to depol block
+# PBTB is really PBT depolarisation block for all
+# PBP is PB depol block
+# PB is P to depol block
+# TBP is T to depol block
+# BP is all depol block
+# PTBT is P to depol block
+"""
