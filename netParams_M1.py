e1bf7dcf664d3f85a7e4a210f03398f68c1e40e4
Michelle Gee <78487248+michmgee@users.noreply.github.com>
2024-06-18 21:13:51 -0400

    Update netParams_M1.py
    revert back to previous version

--- netParams_M1.py
+++ netParams_M1.py
@@ -4,28 +4,51 @@ Created in April 2021
 
 @author: sgupta
 """
-import os
-os.system('nrnivmodl mod')
-from netpyne import sim
-from neuron import h
 
-def fi(seg):
-    '''setting steady state RMP for each neuron'''
-    isum = 0
-    isum = (seg.ina if h.ismembrane('na_ion') else 0) + (seg.ik if h.ismembrane('k_ion') else 0) + (seg.ica if h.ismembrane('ca_ion') else 0) + (seg.iother if h.ismembrane('other_ion') else 0)
-    seg.e_pas = cfg.hParams['v_init']+isum/seg.g_pas 
-    if h.ismembrane('cadad'):
-        seg.cainf_cadad = seg.cai - ((- (10000) * seg.ica / (2 * h.FARADAY * seg.depth_cadad)) * seg.taur_cadad)
-    print(cfg.cellnum, seg.e_pas)
+from netpyne import specs, sim
+import numpy as np
+import csv
 
-def simSim (np0, sc0):
-    sim.create(netParams=np0, simConfig=sc0)
-    fih = [h.FInitializeHandler(2, lambda: fi(sim.net.cells[0].secs.soma.hObj(0.5)))]
-    print('BEFORE simulate')
-    sim.simulate()
-    sim.saveData()
-    sim.analyze()
-    print('AFTER save')
+#Things to change
+cellfile = 'red_tdata_all_15.csv'
 
-simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams_M1.py')
-simSim(netParams, simConfig)
+
+netParams = specs.NetParams()
+try:
+    from __main__ import cfg  # import SimConfig object with params from parent module
+except:
+    from cfg import cfg  # if no simConfig in parent module, import directly from cfg.py:cfg
+
+netParams.defaultThreshold = -10 # defining threshold for spike detection
+
+# order in genemod MUST be preserved to match cell_identities channel order
+genemod = {'ch_Hcn3_cp11':{'gHCN3bar': cfg.h3},  'ch_Hcn1_cp9':{'gHCN1bar': cfg.h1},
+           'ch_Cacna1g_cp41':{'gCav3_1bar': cfg.c1g},        'ch_Cacna1a_cp5':{'gCav2_1bar': cfg.c1a},
+           'ch_Cacna1d_md150284':{'pcaLbar': cfg.c1d},  'ch_Hcn4_cp12':{'gHCN4bar': cfg.h4},
+            'ch_Cacna1c_cp3':{'gLbar': cfg.c1c},         'ch_Cacna1i_md279':{'gcabar': cfg.c1i},           
+            'ch_Kcna1ab1_md80769':{'gbar': cfg.ka},        'ch_Kcnj3_md2488':{'gbar': cfg.kj},           
+            'ch_Cacna1b_cp6':{'gCav2_2bar': cfg.c1b},  'ch_Hcn2_cp10':{'gHCN2bar': cfg.h2},            
+            'ch_Kcnc1_rothman':{'gbar': cfg.kc, 'phi':cfg.phi}, 'ch_Scn1a_cp35':{'gNabar': cfg.na}  }     
+
+cell_identities = np.bool_(np.transpose(np.genfromtxt(cellfile, delimiter=','))) # was 15_m2l.csv before
+cell = cell_identities[cfg.cellnum]
+
+## Cell parameters/rules
+CEL = {'secs': {}}
+
+CEL['secs']['soma'] = {'geom': {'diam': cfg.sze, 'L': cfg.sze, 'Ra': 35.4, 'cm':1}, 'mechs':  {'pas' : {'g': 0.00078, 'e': -65} }} # Ra: 35.4, 'e': -65
+                                                                            
+
+for mod,onoff in zip(genemod,cell):
+    if onoff:
+        CEL['secs']['soma']['mechs'][mod]=genemod[mod]
+netParams.cellParams['CEL'] = CEL
+netParams.popParams['U'] = {'cellType': 'CEL', 'numCells': 1} # leave this as 1
+
+if cfg.stim == 'IClamp':
+    netParams.stimSourceParams['iclamp'] = {'type': 'IClamp', 'amp': cfg.amp, 'dur': 400, 'delay': 100} 
+    netParams.stimTargetParams['iclamp->CEL'] = {'source': 'iclamp', 'conds': {'cellType': 'CEL'}, 'sec': 'soma', 'loc': 0.5}
+elif cfg.stim == 'VClamp':
+    netParams.stimSourceParams['vclamp'] = {'type': 'VClamp', 'dur': [100,400,500], 'amp': [cfg.hParams['v_init'],cfg.vc,cfg.hParams['v_init'] ]} 
+    netParams.stimTargetParams['vclamp->CEL'] = {'source': 'vclamp', 'conds': {'cellType': 'CEL'}, 'sec': 'soma', 'loc': 0.5}
+
